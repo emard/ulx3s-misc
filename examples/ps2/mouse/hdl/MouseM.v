c8cace0bb7011a128d9a3e373a43a201a70c9331
emard <vordah@gmail.com>
2019-05-11 13:30:29 +0200

    mousem, cleanup and parametrized

--- examples/ps2/mouse/hdl/MouseM.v
+++ examples/ps2/mouse/hdl/MouseM.v
@@ -25,13 +25,20 @@ CONNECTION WITH THE DEALINGS IN OR USE OR PERFORMANCE OF THE SOFTWARE.*/
 // EMARD added z-axis (wheel) 10.05.2019
 // https://isdaman.com/alsos/hardware/mouse/ps2interface.htm
 
-module MouseM(
-  input clk, rst,
+module MouseM
+#(
+  parameter c_x_bits = 11, // >= 8
+  parameter c_y_bits = 11, // >= 8
+  parameter c_z_bits = 11  // >= 4
+)
+(
+  input clk, reset,
   inout msclk, msdat,
-  output [39:0] out);
-
-  reg [10:0] x, y, z;
-  reg [2:0] btns;
+  output reg [c_x_bits-1:0] x,
+  output reg [c_y_bits-1:0] y,
+  output reg [c_z_bits-1:0] z,
+  output reg [2:0] btn
+);
   reg [2:0] sent;
   reg [41:0] rx;
   reg [9:0] tx;
@@ -40,7 +47,9 @@ module MouseM(
   reg req;
   wire shift, endbit, endcount, done, run;
   wire [8:0] cmd;  //including odd tx parity bit
-  wire [10:0] dx, dy, dz;
+  wire [c_x_bits-1:0] dx;
+  wire [c_y_bits-1:0] dy;
+  wire [c_z_bits-1:0] dz;
 
 // 322222222221111111111 (scroll mouse z and rx parity p ignored)
 // 0987654321098765432109876543210   X, Y = overflow
@@ -53,30 +62,32 @@ module MouseM(
    (sent == 2) ? 9'h0C8 : (sent == 4) ? 9'h064 : (sent == 6) ? 9'h150 : 9'h1F3;
   assign endcount = (count[14:12] == 3'b111);  // more than 11*100uS @25MHz
   assign shift = ~req & (filter == 6'b100000);  //low for 200nS @25MHz
-  assign endbit = run ? ~rx[0] : ~rx[21]; //first received bit is 1, shifts right and acts as terminator
+  // fireset bit that enters rx at MSB is 1 and it is shifted to the right
+  // when this bit reaches the position in rx, it indicates end of transmission
+  assign endbit = run ? ~rx[0] : ~rx[$bits(rx)-21];
   assign done = endbit & endcount & ~req;
-  assign dx = {{3{rx[5]}}, rx[7] ? 8'b0 : rx[19:12]};  //sign+overfl
-  assign dy = {{3{rx[6]}}, rx[8] ? 8'b0 : rx[30:23]};  //sign+overfl
-  assign dz = {{7{rx[37]}}, rx[37:34]};  //sign,wheel
+  assign dx = {{($bits(dx)-8){rx[5]}}, rx[7] ? 8'b0 : rx[19:12]};  //sign+overfl
+  assign dy = {{($bits(dy)-8){rx[6]}}, rx[8] ? 8'b0 : rx[30:23]};  //sign+overfl
+  assign dz = {{($bits(dz)-3){rx[37]}}, rx[37:34]};  //sign,wheel
 //  assign out = {run,  // full debug
 //    run ? {rx[25:0], endbit} : {rx[30:10], endbit, sent, tx[0], ~req}};
 //  assign out = {run,  // debug then normal
 //    run ? {btns, 2'b0, y, 2'b0, x} : {rx[30:10], sent, endbit, tx[0], ~req}};
-  assign out = {1'b0, z, run, btns, 1'b0, y, 1'b0, x}; // normal
+  assign out = {1'b0, z, run, btn, 1'b0, y, 1'b0, x}; // normal
   assign msclk = req ? 1'b0 : 1'bz;  //bidir clk/request
   assign msdat = ~tx[0] ? 1'b0 : 1'bz;  //bidir data
 
   always @ (posedge clk) begin
     filter <= {filter[4:0], msclk};
-    count <= (~rst | shift | endcount) ? 0 : count+1;
-    req <= rst & ~run & (req ^ endcount);
-    sent <= ~rst ? 0 : (done & ~run) ? sent+1 : sent;
-    tx <= (~rst | run) ? 10'h3FF : req ? {cmd, 1'b0} : shift ? {1'b1, tx[9:1]} : tx;
-    rx <= (~rst | done) ? 42'h3FFFFFFFFFF : (shift & ~endbit) ? {msdat, rx[41:1]} : rx;
+    count <= (reset | shift | endcount) ? 0 : count+1;
+    req <= ~reset & ~run & (req ^ endcount);
+    sent <= reset ? 0 : (done & ~run) ? sent+1 : sent;
+    tx <= (reset | run) ? {$bits(tx){1'b1}} : req ? {cmd, 1'b0} : shift ? {1'b1, tx[$bits(tx)-1:1]} : tx;
+    rx <= (reset | done) ? {$bits(rx){1'b1}} : (shift & ~endbit) ? {msdat, rx[$bits(rx)-1:1]} : rx;
     x <= ~run ? 0 : done ? x + dx : x;
     y <= ~run ? 0 : done ? y + dy : y;
     z <= ~run ? 0 : done ? z + dz : z;
-    btns <= ~run ? 0 : done ? {rx[1], rx[3], rx[2]} : btns;
+    btn <= ~run ? 0 : done ? rx[3:1] : btn;
   end
 
 endmodule
