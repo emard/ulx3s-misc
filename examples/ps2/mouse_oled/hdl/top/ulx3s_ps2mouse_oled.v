b769cae40b275c861fea6a79dfb42414de5c1331
emard <vordah@gmail.com>
2019-05-26 10:44:27 +0200

    ps2 mouse oled - in toplevel there's now a choice minimig/oberon

--- examples/ps2/mouse_oled/hdl/top/ulx3s_ps2mouse_oled.v
+++ examples/ps2/mouse_oled/hdl/top/ulx3s_ps2mouse_oled.v
@@ -1,4 +1,7 @@
 module ulx3s_ps2mouse_oled
+#(
+  parameter mousecore = 1 // 0-minimig 1-oberon
+)
 (
   input clk_25mhz,
   input [6:0] btn,
@@ -21,13 +24,6 @@ module ulx3s_ps2mouse_oled
     assign usb_fpga_pu_dp = 1'b1;
     assign usb_fpga_pu_dn = 1'b1;
 
-    wire ps2mdat_in, ps2mclk_in, ps2mdat_out, ps2mclk_out;
-    
-    assign usb_fpga_dp = ps2mclk_out ? 1'bz : 1'b0;
-    assign usb_fpga_dn = ps2mdat_out ? 1'bz : 1'b0;
-    assign ps2mclk_in = usb_fpga_dp;
-    assign ps2mdat_in = usb_fpga_dn;
-
     reg [19:0] reset_counter;
     always @(posedge clk)
     begin
@@ -40,20 +36,57 @@ module ulx3s_ps2mouse_oled
     assign reset = reset_counter[19];
     assign led[0] = reset;
 
-    wire [7:0] mouse_x, mouse_y;
-    ps2mouse
-    ps2mouse_inst
-    (
-      .clk(clk),
-      .reset(reset),
-      .ps2mdati(ps2mdat_in),
-      .ps2mclki(ps2mclk_in),
-      .ps2mdato(ps2mdat_out),
-      .ps2mclko(ps2mclk_out),
-      .xcount(mouse_x),
-      .ycount(mouse_y),
-      .btn(led[3:1])
-    );
+    wire [2:0] mouse_btn;
+    wire [9:0] mouse_x, mouse_y, mouse_z;
+
+    generate
+      if(mousecore == 0) // using amiga core
+      begin
+        wire ps2mdat_in, ps2mclk_in, ps2mdat_out, ps2mclk_out;
+        assign usb_fpga_dp = ps2mclk_out ? 1'bz : 1'b0;
+        assign usb_fpga_dn = ps2mdat_out ? 1'bz : 1'b0;
+        assign ps2mclk_in = usb_fpga_dp;
+        assign ps2mdat_in = usb_fpga_dn;
+        ps2mouse
+        #(
+          .c_x_bits(10),
+          .c_y_bits(10)
+        )
+        ps2mouse_amiga_inst
+        (
+          .clk(clk),
+          .reset(reset),
+          .ps2mdati(ps2mdat_in),
+          .ps2mclki(ps2mclk_in),
+          .ps2mdato(ps2mdat_out),
+          .ps2mclko(ps2mclk_out),
+          .xcount(mouse_x),
+          .ycount(mouse_y),
+          .btn(mouse_btn)
+        );
+      end
+      if(mousecore == 1) // using oberon core
+      begin
+        mousem
+        #(
+          .c_x_bits(10),
+          .c_y_bits(10),
+          .c_z_bits(10)
+        )
+        ps2mouse_oberon_inst
+        (
+          .clk(clk),
+          .reset(reset),
+          .msclk(usb_fpga_dp),
+          .msdat(usb_fpga_dn),
+          .x(mouse_x),
+          .y(mouse_y),
+          .z(mouse_z),
+          .btn(mouse_btn)
+        );
+      end
+    endgenerate
+
     assign led[7:6] = mouse_y[1:0];
     assign led[5:4] = mouse_x[1:0];
 
