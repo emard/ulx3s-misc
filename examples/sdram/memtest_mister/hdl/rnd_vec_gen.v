200801e490fc029c4f6cb32e4cb7156835b931d1
emard <vordah@gmail.com>
2019-08-31 10:43:51 +0200

    memtest rnd_vec_gen.v: fixed to compile

--- examples/sdram/memtest_mister/hdl/rnd_vec_gen.v
+++ examples/sdram/memtest_mister/hdl/rnd_vec_gen.v
@@ -1,5 +1,4 @@
 module rnd_vec_gen(
-
 	clk,
 
 	init,
@@ -7,7 +6,6 @@ module rnd_vec_gen(
 	save,
 	restore,
 	next,
-
 
 	out
 );
@@ -17,31 +15,24 @@ parameter OUT_SIZE = 16; // size of output port, independent of LFSR register si
 parameter LFSR_LENGTH   = 55; // LFSR
 parameter LFSR_FEEDBACK = 24; //     definition
 
-
 	input clk;
 
 	input init; // positive initialization strobe, synchronous to clock, its length will determine initial state
 
 	input save,restore,next; // strobes for required events: positive, one clock cycle long
 
-
 	reg init2;
-
 
 	output  [OUT_SIZE-1:0] out;
 	wire    [OUT_SIZE-1:0] out;
 
-
 	reg [OUT_SIZE-1:0] rndbase_main  [0:LFSR_LENGTH-1];
 	reg [OUT_SIZE-1:0] rndbase_store [0:LFSR_LENGTH-1];
 
-
-
-
 	assign out = rndbase_main[0];
 
-/*
-				integer i;
+	integer i;
+	integer j;
 
 	always @(posedge clk)
 	begin
@@ -54,35 +45,52 @@ parameter LFSR_FEEDBACK = 24; //     definition
 		end
 		else if( init && init2 ) // continue of initialization
 		begin
-//			shift_lfsr;
+			shift_lfsr;
 		end
 		else // no init, normal work
 		begin
 
 			if( restore ) // restore event: higher priority
 			begin
-//				for(i=0;i<LFSR_LENGTH;i=i+1)
-//					rndbase_main[i] <= rndbase_store[i];
+				for(i=0;i<LFSR_LENGTH;i=i+1)
+					rndbase_main[i] <= rndbase_store[i];
 			end
 			else
 			begin
 				if( next ) // step to next value
 				begin
-//					shift_lfsr;
+					shift_lfsr;
 				end
 
 				if( save ) // save current state
 				begin
-//					for(j=0;j<LFSR_LENGTH;j=j+1)
-//						rndbase_store[j] <= rndbase_main[j];
+					for(j=0;j<LFSR_LENGTH;j=j+1)
+						rndbase_store[j] <= rndbase_main[j];
 				end
 			end
 		end
 	end
-*/
 
+	reg [OUT_SIZE-1:0] sum;
+	reg [LFSR_LENGTH-1:0] lsbs;
+	integer ti;
 
-/*	function [LFSR_LENGTH-1:0] shift_lfsr;
+	task shift_lfsr;
+	begin
+		for(ti=0;ti<LFSR_LENGTH;ti=ti+1)
+			lsbs[ti] <= rndbase_main[ti][0];
+
+		sum = rndbase_main[LFSR_LENGTH-1] + rndbase_main[LFSR_FEEDBACK-1];
+
+		for(ti=1;ti<LFSR_LENGTH;ti=ti+1)
+			rndbase_main[ti] <= rndbase_main[ti-1];
+
+		rndbase_main[0] <= { sum[OUT_SIZE-1:1], (|lsbs)?sum[0]:1'b1 };
+	end
+	endtask
+
+/*
+	function [LFSR_LENGTH-1:0] shift_lfsr;
 
 		input [LFSR_LENGTH-1:0] old_lfsr;
 
@@ -95,31 +103,4 @@ parameter LFSR_FEEDBACK = 24; //     definition
 	endfunction
 */
 
-/*
-	task shift_lfsr;
-	begin
-		reg [OUT_SIZE-1:0] sum;
-		reg [LFSR_LENGTH-1:0] lsbs;
-
-		integer i;
-
-		for(i=0;i<LFSR_LENGTH;i=i+1)
-			lsbs[i] <= rndbase_main[i][0];
-
-		sum = rndbase_main[LFSR_LENGTH-1] + rndbase_main[LFSR_FEEDBACK-1];
-
-//		integer j;
-		for(i=1;i<LFSR_LENGTH;i=i+1)
-			rndbase_main[i] <= rndbase_main[i-1];
-
-		rndbase_main[0] <= { sum[OUT_SIZE-1:1], (|lsbs)?sum[0]:1'b1 };
-	end
-	endtask
-*/
-
 endmodule
-
-
-
-
-
