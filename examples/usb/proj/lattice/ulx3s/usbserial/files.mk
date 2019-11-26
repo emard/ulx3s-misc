a49a91ca8f706a467d8c29a6fd60e39fa57a2cb9
emard <vordah@gmail.com>
2019-11-26 15:07:23 +0100

    usbcdc fix to compile (untested)

--- examples/usb/proj/lattice/ulx3s/usbserial/files.mk
+++ examples/usb/proj/lattice/ulx3s/usbserial/files.mk
@@ -11,8 +11,7 @@ VHDL_FILES = $(TOP_MODULE_FILE) \
   ../../../../usbcdc/usb_init.vhd \
   ../../../../usbcdc/usb_control.vhd \
   ../../../../usb11_phy_vhdl/usb_phy.vhd \
-  ../../../../usb11_phy_vhdl/usb_rx_phy_48MHz.vhd \
-  ../../../../usb11_phy_vhdl/usb_rx_phy_emard.vhd \
+  ../../../../usb11_phy_vhdl/usb_rx_phy.vhd \
   ../../../../usb11_phy_vhdl/usb_tx_phy.vhd \
   ../../../../../oled/hdl/ssd1331_hex_vhdl/oled_hex_decoder.vhd \
   ../../../../../oled/hdl/ssd1331_hex_vhdl/oled_font_pack.vhd \
