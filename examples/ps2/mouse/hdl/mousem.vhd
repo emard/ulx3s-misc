2a3319ae0523519923421450a4a13e8fd8bcd57a
emard <vordah@gmail.com>
2019-12-09 18:47:49 +0100

    mousem.vhd: option to invert Y

--- examples/ps2/mouse/hdl/mousem.vhd
+++ examples/ps2/mouse/hdl/mousem.vhd
@@ -31,16 +31,18 @@ use ieee.std_logic_arith.all;
 entity mousem is
   generic
   (
-    c_x_bits: integer range 8 to 11 := 8;
-    c_y_bits: integer range 8 to 11 := 8;
-    c_z_bits: integer range 4 to 11 := 4;
-    c_hotplug: integer range 0 to 1 := 1; -- 1:mouse hotplug detection 0:no mouse hotplug (save LUTs)
-    c_z_ena:  integer range 0 to  1 := 1  -- 1:yes wheel, 0:not wheel
+    c_x_bits:   integer range 8 to 11 := 8;
+    c_y_bits:   integer range 8 to 11 := 8;
+    c_y_neg:    integer range 0 to  1 := 0;
+    c_z_bits:   integer range 4 to 11 := 4;
+    c_z_ena:    integer range 0 to  1 := 1; -- 1:yes wheel, 0:not wheel
+    c_hotplug:  integer range 0 to  1 := 1  -- 1:mouse hotplug detection 0:no mouse hotplug (save LUTs)
   );
   port
   (
-    clk, ps2m_reset: in std_logic;
+    clk: in std_logic;
     clk_ena: in std_logic := '1';
+    ps2m_reset: in std_logic := '0';
     ps2m_clk, ps2m_dat: inout std_logic;
     update: out std_logic;
     x, dx: out std_logic_vector(c_x_bits-1 downto 0);
@@ -171,9 +173,16 @@ begin
   x_next <= (others => '0') when run = '0'
         else r_x + s_dx when done = '1'
         else r_x;
+  G_not_invert_y: if C_y_neg = 0 generate
   y_next <= (others => '0') when run = '0'
-        else r_y - s_dy when done = '1'
+        else r_y - s_dy when done = '1' -- PS2 mouse sends negative dy
         else r_y;
+  end generate;
+  G_yes_invert_y: if C_y_neg = 1 generate
+  y_next <= (others => '0') when run = '0'
+        else r_y + s_dy when done = '1' -- PS2 mouse sends negative dy
+        else r_y;
+  end generate;
   z_next <= (others => '0') when run = '0'
         else r_z - s_dz when done = '1'
         else r_z;
