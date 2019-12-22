15bf4cbad642efbab24b50f44e4e897a10a7b1a6
emard <vordah@gmail.com>
2019-12-22 13:38:37 +0100

    PS/2 mouse to oled example works again

--- examples/ps2/mouse_oled/hdl/top/ulx3s_ps2mouse_oled.v
+++ examples/ps2/mouse_oled/hdl/top/ulx3s_ps2mouse_oled.v
@@ -6,7 +6,7 @@ module ulx3s_ps2mouse_oled
   input clk_25mhz,
   input [6:0] btn,
   output [7:0] led,
-  inout usb_fpga_dp, usb_fpga_dn,
+  inout usb_fpga_bd_dp, usb_fpga_bd_dn,
   output usb_fpga_pu_dp, usb_fpga_pu_dn,
   output wire oled_csn,
   output wire oled_clk,
@@ -43,10 +43,10 @@ module ulx3s_ps2mouse_oled
       if(mousecore == 0) // using amiga core
       begin
         wire ps2mdat_in, ps2mclk_in, ps2mdat_out, ps2mclk_out;
-        assign usb_fpga_dp = ps2mclk_out ? 1'bz : 1'b0;
-        assign usb_fpga_dn = ps2mdat_out ? 1'bz : 1'b0;
-        assign ps2mclk_in = usb_fpga_dp;
-        assign ps2mdat_in = usb_fpga_dn;
+        assign usb_fpga_bd_dp = ps2mclk_out ? 1'bz : 1'b0;
+        assign usb_fpga_bd_dn = ps2mdat_out ? 1'bz : 1'b0;
+        assign ps2mclk_in = usb_fpga_bd_dp;
+        assign ps2mdat_in = usb_fpga_bd_dn;
         ps2mouse
         #(
           .c_x_bits(10),
@@ -79,8 +79,8 @@ module ulx3s_ps2mouse_oled
           .clk(clk),
           .clk_ena(1'b1),
           .ps2m_reset(reset),
-          .ps2m_clk(usb_fpga_dp),
-          .ps2m_dat(usb_fpga_dn),
+          .ps2m_clk(usb_fpga_bd_dp),
+          .ps2m_dat(usb_fpga_bd_dn),
           .x(mouse_x),
           .y(mouse_y),
           .z(mouse_z),
