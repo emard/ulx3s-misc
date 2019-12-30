042f231810f586736f596275db8accddc674c2f6
emard <vordah@gmail.com>
2019-12-30 16:35:22 +0100

    ffm example for onchip blink

---
+++ examples/onchip_osc_blink_ffm/README.md
@@ -0,0 +1,4 @@
+# OSCG on-chip oscillator
+
+LED blinky using on-chip oscillator module "OSCG".
+It doesn't need external clock source.
---
+++ examples/onchip_osc_blink_ffm/oscg_blink.v
@@ -0,0 +1,31 @@
+// AUTHOR=EMARD
+// LICENSE=BSD
+
+// blink LED example using on-chip oscillator
+
+module oscg_blink
+(
+  output [3:1] led
+);
+  parameter oscillator_divider = 12; // f=310MHz/divider = 310/12 = 25.8 MHz
+  parameter counter_bits = 28;
+
+  wire clk;
+
+  // on-chip oscillator
+  OSCG
+  #(
+    .DIV(oscillator_divider) // freq = 310MHz/div, div = 2..128
+  )
+  oscg_instance
+  (
+    .OSC(clk)
+  );
+
+  reg [counter_bits-1:0] counter;
+  always @(posedge clk)
+    counter <= counter + 1;
+  
+  assign led = counter[counter_bits-1:counter_bits-3];
+
+endmodule
