cfdccc448fd03e96e05caf89ee1adc03ca7eaacb
emard <vordah@gmail.com>
2020-02-16 20:16:12 +0100

    fix ssd1331 checkered demo

--- examples/oled/hdl/ssd1331_video_verilog/top/top_checkered.v
+++ examples/oled/hdl/ssd1331_video_verilog/top/top_checkered.v
@@ -13,6 +13,7 @@ module top_checkered
     assign wifi_gpio0 = btn[0];
     parameter C_color_bits = 16; // 8 or 16
 
+/*
     wire clk, locked;
     pll
     pll_inst
@@ -21,6 +22,8 @@ module top_checkered
         .clko(clk), // 12.5 MHz
         .locked(locked)
     );
+*/
+    wire clk = clk_25mhz;
 
     wire [6:0] x;
     wire [5:0] y;
@@ -39,8 +42,8 @@ module top_checkered
     begin
     //                  checkered      red   green   blue     red     green blue
     wire [15:0] color = x[3] ^ y[3] ? {5'd0, x[6:1], 5'd0} : {y[5:1], 6'd0, 5'd0};
-    localparam C_init_file = "oled_init_16bit.mem";
-    //localparam C_init_file = "oled_init_xflip_16bit.mem";
+    //localparam C_init_file = "oled_init_16bit.mem";
+    localparam C_init_file = "ssd1351_init_xflip_16bit.mem";
     //localparam C_init_file = "oled_init_yflip_16bit.mem";
     //localparam C_init_file = "oled_init_xyflip_16bit.mem";
     end
@@ -48,8 +51,8 @@ module top_checkered
     
     oled_video
     #(
-        .C_init_file(C_init_file),
-        .C_color_bits(C_color_bits)
+        .c_init_file(C_init_file),
+        .c_color_bits(C_color_bits)
     )
     oled_video_inst
     (
@@ -57,11 +60,11 @@ module top_checkered
         .x(x),
         .y(y),
         .color(color),
-        .oled_csn(oled_csn),
-        .oled_clk(oled_clk),
-        .oled_mosi(oled_mosi),
-        .oled_dc(oled_dc),
-        .oled_resn(oled_resn)
+        .spi_csn(oled_csn),
+        .spi_clk(oled_clk),
+        .spi_mosi(oled_mosi),
+        .spi_dc(oled_dc),
+        .spi_resn(oled_resn)
     );
 
 endmodule
