05f896994b7680517894b39435ecfb78c1540b09
emard <vordah@gmail.com>
2020-02-16 15:49:55 +0100

    st7789 toplevel updated

--- examples/lcd_st7789/hdl/st7789_video_verilog/top/top_hex_demo.v
+++ examples/lcd_st7789/hdl/st7789_video_verilog/top/top_hex_demo.v
@@ -84,11 +84,12 @@ module top_hex_demo
         .y(y),
         .next_pixel(next_pixel),
         .color(R_color),
-        .oled_csn(oled_csn),
-        .oled_clk(oled_clk),
-        .oled_mosi(oled_mosi),
-        .oled_dc(oled_dc),
-        .oled_resn(oled_resn)
+        //.spi_csn(oled_csn),
+        .spi_clk(oled_clk),
+        .spi_mosi(oled_mosi),
+        .spi_dc(oled_dc),
+        .spi_resn(oled_resn)
     );
+    assign oled_csn = 1; // CSN is connected to BLK (backlight enable pin)
 
 endmodule
