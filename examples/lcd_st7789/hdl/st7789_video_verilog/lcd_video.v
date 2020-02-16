a8d9294ea6a8674ba17d4e80e68dd83b82907c2a
emard <vordah@gmail.com>
2020-02-16 10:20:13 +0100

    st7789 prevent clocking out NOPs during arg parsing and delays

--- examples/lcd_st7789/hdl/st7789_video_verilog/lcd_video.v
+++ examples/lcd_st7789/hdl/st7789_video_verilog/lcd_video.v
@@ -2,8 +2,6 @@
 // AUTHORS=EMARD,MMICKO and Lawrie Griffiths
 // LICENSE=BSD
 
-// FIXME after reset needs 5ms delay before commands
-
 module lcd_video #(
   parameter c_clk_mhz = 25, // MHz clk freq (125 MHz max for st7789)
   parameter c_reset_us = 150000,  // us holding hardware reset
@@ -15,7 +13,8 @@ module lcd_video #(
   // file name is relative to directory path in which verilog compiler is running
   // screen can be also XY flipped and/or rotated from this init file
   parameter c_init_file = "st7789_init.mem",
-  parameter c_init_size = 38 // bytes in init file
+  parameter c_init_size = 38, // bytes in init file
+  parameter c_nop = 8'h00 // during delays and arg parsing: set data to this NOP
 ) (
   input  wire clk, // SPI display clock rate will be half of this clock rate
   input  wire reset,
@@ -39,7 +38,7 @@ module lcd_video #(
   end
 
   reg [10:0] init_cnt;
-  reg [7:0] data;
+  reg [7:0] data = c_nop;
   reg dc = 1;
   reg byte_toggle; // alternates data byte for 16-bit mode
   reg init = 1;
@@ -49,11 +48,12 @@ module lcd_video #(
   reg delay_set = 0;
   reg [7:0] last_cmd;
   reg resn = 0;
+  reg clken = 0;
 
   assign oled_resn = resn;          // Reset is High, Low, High for first 3 cycles
   assign oled_csn = resn;           // Connected to backlight
   assign oled_dc = dc;              // 0 for commands, 1 for command parameters and data
-  assign oled_clk = init_cnt[0];    // SPI Mode 2
+  assign oled_clk = init_cnt[0] | ~clken; // no clock during delay
   assign oled_mosi = data[7];       // Shift out data
 
   // The next byte in the initialisation sequence
@@ -72,6 +72,8 @@ module lcd_video #(
       y <= 0;
       byte_toggle <= 0;
       arg <= 1; // after reset, before commands take delay from init sequence
+      data <= c_nop;
+      clken <= 0;
     end else if (delay_cnt[$bits(delay_cnt)-1] == 0) begin // Delay
       delay_cnt <= delay_cnt - 1;
       resn <= 1;
@@ -82,20 +84,24 @@ module lcd_video #(
           dc <= 0;
           arg <= arg + 1;
           if (arg == 0) begin // New command
-            data <=  0; // No NOP
+            data <= c_nop; // No NOP
+            clken <= 0;
             last_cmd <= next_byte;
           end else if (arg == 1) begin // numArgs and delay_set
             num_args <= next_byte[4:0];
             delay_set <= next_byte[7];
             if (next_byte == 0) arg <= 0; // No args or delay
             data <= last_cmd;
+            clken <= 1;
           end else if (arg <= num_args + 1) begin // argument
             data <= next_byte;
+            clken <= 1;
             dc <= 1;
             if (arg == num_args + 1 && !delay_set) arg <= 0;
           end else if (delay_set) begin // delay
             delay_cnt <= c_clk_mhz << (next_byte[4:0]); // 2^n us delay
-            data <= 0;
+            data <= c_nop;
+            clken <= 0;
             delay_set <= 0;
             arg <= 0;
           end
@@ -103,6 +109,7 @@ module lcd_video #(
           byte_toggle <= ~byte_toggle;
           dc <= 1;
           data <= byte_toggle ? color[7:0] : color[15:8];
+          clken <= 1;
           if (byte_toggle) begin
             next_pixel <= 1;
             if (x == c_x_size-1) begin
