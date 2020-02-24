10a8fdd153c239f0f70a4eb0411192d59033c983
emard <vordah@gmail.com>
2020-02-24 19:52:30 +0100

    ch376 serial descriptive resonse

--- examples/usb/ch376/python/ch376.py
+++ examples/usb/ch376/python/ch376.py
@@ -3,10 +3,12 @@ import serial
 
 def test():
  s = serial.Serial("/dev/ttyUSB0", 9600, timeout=1, xonxoff=0, rtscts=0)
+ 
+ msg = ["FAIL", "OK"]
 
  for value in bytearray([0x5A, 0xF0, 0xC3]):
    s.write([0x57, 0xAB, 0x06, value])
    r = s.read(1)
-   print("(0x%02X expected) reply: 0x%02X" % (value ^ 0xFF, r[0]))
+   print("(0x%02X expected) reply: 0x%02X %s" % (value ^ 0xFF, r[0], msg[(value ^ 0xFF) == r[0]]))
 
 test()
