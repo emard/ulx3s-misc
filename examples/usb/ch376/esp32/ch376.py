38cb05885f90de2f9338f77ed06b17acef03dcac
emard <vordah@gmail.com>
2020-02-27 10:36:04 +0100

    ch376 creates usb traffic at 1.5 Mbps but no hid reports yet

--- examples/usb/ch376/esp32/ch376.py
+++ examples/usb/ch376/esp32/ch376.py
@@ -43,9 +43,16 @@ class ch376:
     self.hwspi.write(bytearray([0x4D]))
     self.led.off()
 
+  # sets remote address
   def set_address(self,addr:int):
     self.led.on()
     self.hwspi.write(bytearray([0x45,addr]))
+    self.led.off()
+
+  # sets our address
+  def set_our_address(self,addr:int):
+    self.led.on()
+    self.hwspi.write(bytearray([0x13,addr]))
     self.led.off()
 
   def set_usb_speed(self,speed:int):
@@ -53,10 +60,22 @@ class ch376:
     self.hwspi.write(bytearray([0x04,speed]))
     self.led.off()
 
+  def set_usb_low_speed(self):
+    self.led.on()
+    self.hwspi.write(bytearray([0x0B,0x17,0xD8]))
+    self.led.off()
+
   def set_config(self,config:int):
     self.led.on()
     self.hwspi.write(bytearray([0x49,config]))
     self.led.off()
+
+  def get_status(self) -> int:
+    self.led.on()
+    self.hwspi.write(bytearray([0x22]))
+    status = self.hwspi.read(1)[0]
+    self.led.off()
+    return status
 
   def rd_usb_data0(self):
     self.led.on()
@@ -80,6 +99,17 @@ class ch376:
     self.led.off()
     return data
 
+  def wr_usb_data(self,buffer):
+    self.led.on()
+    self.hwspi.write(bytearray([0x2C,len(buffer)]))
+    self.hwspi.write(buffer)
+    self.led.off()
+
+  def issue_token_x(self,token:int,x:int):
+    self.led.on()
+    self.hwspi.write(bytearray([0x4E,token,x]))
+    self.led.off()
+
   def test_connect(self) -> int:
     self.led.on()
     self.hwspi.write(bytearray([0x16]))
@@ -102,14 +132,31 @@ def test():
   u.set_usb_mode(7) # reset and host mode, mouse turns bottom LED ON
   sleep_ms(20)
   u.set_usb_mode(6) # release from reset
+  
   u.set_usb_speed(2) # low speed 1.5 Mbps
-  #sleep_ms(100)
   u.auto_setup()
+
+  #u.set_usb_low_speed()
   #u.set_address(1)
+  #u.set_our_address(1)
   #u.set_config(1)
-  #print("%02X" % u.test_connect())
-  for i in range(10):
+  #print("stat %02X" % u.get_status())
+  #print("stat %02X" % u.get_status())
+  #u.wr_usb_data(bytearray([0x21,0x0B,0,0,0,0,0,0])) # BOOTP compatibility mode
+  #u.issue_token_x(0x80,0x0D)
+  #sleep_ms(1)
+  #print("stat %02X" % u.get_status())
+
+  token = 0
+  i = 0
+  while True:
+    u.issue_token_x(token,0x19)
     sleep_ms(1)
-    print(u.rd_usb_data0())
+    u.get_status()
+    token ^= 0x80
+    d = u.rd_usb_data0()
+    if (i & 1023) == 0:
+      print(len(d),d)
+    i+=1
 
 test()
