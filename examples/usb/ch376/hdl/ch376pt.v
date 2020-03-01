6900caf31bbc3e2d0af7fbfba7759d7c4100b71d
emard <vordah@gmail.com>
2020-03-01 21:02:33 +0100

    ch376: SPI mode enabled in bitstream loaded from SPI FLASH

--- examples/usb/ch376/hdl/ch376pt.v
+++ examples/usb/ch376/hdl/ch376pt.v
@@ -30,21 +30,23 @@ assign ftdi_rxd = wifi_txd;
 //      D1 = GP[27]  GND = GND
 //      D2 = GN[26]  VCC = +5V
 // CSN  D3 = GP[26]  INT = GN[25]
-// BUSY D4 = GP[25]  AO  = GN[24]
+// BUSY D4 = GP[25]  A0  = GN[24] 1
 // SCK  D5 = GP[24]  CS  = GN[23] 1
 // MOSI D6 = GP[23]  RD  = GN[22] 0
 // MISO D7 = GP[22]  WR  = GN[21] 0
 
 // see https://github.com/djuseeq/Ch376msc
 
-// enable CH376 SPI mode (this GN doesn't actually work)
-// SPI setting pins have to be hardwired because
+// enable CH376 SPI mode
 // CH376 scans them only once - early at power-on time.
-// When bitstream is loaded, it will set those pins too late,
-// at that time CH376 has already booted to parallel (not SPI) mode.
+// This works at 25F with bitstream loaded from SPI FLASH.
+// Bitstream uploaded over JTAG is too late, it won't enter
+// SPI mode. In that case, hardwire WR=RD=GND CS=A0=+3.3V
+// assign to 0/1 or use 1'bz as input and FPGA pull-down/pull-up
 assign gn[21]   = 0; // CH376_WR;
 assign gn[22]   = 0; // CH376_RD;
 assign gn[23]   = 1; // CH376_CS;
+assign gn[24]   = 1; // CH376_A0;
 
 wire spi_csn, spi_clk, spi_mosi, spi_miso, spi_int, spi_busy;
 
@@ -75,6 +77,6 @@ assign led[2]   = spi_miso;
 assign led[3]   = spi_mosi;
 assign led[4]   = spi_int;
 assign led[5]   = spi_busy;
-
-assign led[7:6] = 0;
+assign led[6]   = 0;
+assign led[7]   = 0;
 endmodule
