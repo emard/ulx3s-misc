beea273d2625d557679b65197949774687a766b7
emard <vordah@gmail.com>
2020-04-20 00:04:27 +0200

    adc lvds example: updated, untested

--- examples/adc/adc_lcd_lvds/hdl/top/top_hex_480x272.v
+++ examples/adc/adc_lcd_lvds/hdl/top/top_hex_480x272.v
@@ -1,10 +1,13 @@
 module top_hex_480x272
+#(
+  parameter c_counter_bits = 8
+)
 (
-  input clk_25mhz,
-  input [6:0] btn,
-  output [7:0] led,
-  output [27:0] gp, gn,
-  output wifi_gpio0
+  input         clk_25mhz,
+  input   [6:0] btn,
+  output  [7:0] led,
+  inout  [27:0] gp, gn,
+  output        wifi_gpio0
 );
     // wifi_gpio0=1 keeps board from rebooting
     // hold btn0 to let ESP32 take control over the board
@@ -24,23 +27,73 @@ module top_hex_480x272
     wire clk_pixel = clk_25MHz;
     wire clk_shift = clk_175MHz;
     
+    reg [2:0] areg; // 0-7 ADC register
+    reg [c_counter_bits-1:0] counter;
+    reg [4:0] cscounter; // 0-31
     
+    always @(posedge clk_pixel)
+      counter <= counter + 1;
+
+    wire adc_reset = ~btn[0];
+    wire adc_convstn = counter[c_counter_bits-1:c_counter_bits-3] == 6 ? 1 : 0;
+    wire adc_rdn = cscounter[4];
+    wire adc_csn = adc_rdn;
+    wire adc_busy;
+    wire adc_firstdata;
+    wire [15:0] adc_data;
+
+    // *** begin ULX3S-TO-ADC CONNECTION PINOUT ***
+    assign gn[27] = adc_reset;
+    assign gp[26] = adc_rdn;
+    assign gn[26] = adc_csn;
+    assign gn[17] = 1'bz;
+    assign adc_busy = gn[17];
+    assign gn[13] = 1'bz;
+    assign adc_firstdata = gn[13]; // have PULLMODE=DOWN in constraints
+    assign gp[25:18] = 8'hzz;
+    assign gn[25:18] = 8'hzz;
+    generate
+      genvar i;
+      for(i = 0; i < 8; i++)
+      begin
+        assign adc_data[15-i*2] = gp[18+i];
+        assign adc_data[14-i*2] = gn[18+i];
+      end
+    endgenerate
+    // *** end ULX3S-TO-ADC CONNECTION PINOUT ***
 
     parameter C_bits = 256;
     reg [C_bits-1:0] R_display; // something to display
-    always @(posedge clk_25mhz)
+
+    reg [15:0] busycnt = 16'hABCD;
+    reg [1:0] R_adc_busy;
+    reg [15:0] R_adc_data[0:7];
+    always @(posedge clk_pixel)
     begin
-      R_display[0] <= btn[0];
-      R_display[4] <= btn[1];
-      R_display[8] <= btn[2];
-      R_display[12] <= btn[3];
-      R_display[16] <= btn[4];
-      R_display[20] <= btn[5];
-      R_display[24] <= btn[6];
-      R_display[58:52] <= btn;
-      R_display[127:64] <= R_display[127:64] + 1; // shown in next OLED row
-      R_display[31+128:128] <= R_display[31+128:128] + 1; // shown in next OLED row
+      if(R_adc_busy == 2'b10)
+        busycnt <= busycnt + 1;
+      R_adc_busy <= { adc_busy, R_adc_busy[1] };
+      if(adc_busy)
+      begin
+        cscounter <= ~0;
+      end
+      else
+      begin
+        cscounter <= cscounter + 1;
+        if(adc_firstdata)
+          areg <= 0;
+        else
+        begin
+          if(cscounter == 16)
+            R_adc_data[areg] <= adc_data;
+          if(cscounter == 30)
+            areg <= areg + 1;
+        end
+      end
     end
+    
+    always @(posedge clk_pixel)
+      R_display[127:0] <= { busycnt, R_adc_data[6], R_adc_data[5], R_adc_data[4], R_adc_data[3], R_adc_data[2], R_adc_data[1], R_adc_data[0] };
 
     parameter C_color_bits = 16; 
     wire [9:0] x;
@@ -75,7 +128,6 @@ module top_hex_480x272
     wire vga_hsync, vga_vsync, vga_blank;
     vga
     #(
-      // https://github.com/Xilinx/embeddedsw/blob/master/XilinxProcessorIPLib/drivers/video_common/src/xvidc_timings_table.c
       .C_resolution_x(480),
       .C_hsync_front_porch(24),
       .C_hsync_pulse(48),
@@ -94,18 +146,11 @@ module top_hex_480x272
       .test_picture(1'b0), // enable test picture generation
       .beam_x(x),
       .beam_y(y),
-      //.vga_r(vga_r),
-      //.vga_g(vga_g),
-      //.vga_b(vga_b),
       .vga_hsync(vga_hsync),
       .vga_vsync(vga_vsync),
       .vga_blank(vga_blank)
     );
     
-    assign led[7:3] = btn[4:0];
-    assign led[0] = vga_vsync;
-    assign led[1] = vga_hsync;
-    assign led[2] = vga_blank;
 
     // VGA to digital video converter
     wire [3:0] lvds;
@@ -123,8 +168,23 @@ module top_hex_480x272
       .out_lvds(lvds)
     );
     
-    assign gp[6:3] = lvds;
+    assign gp[6:3] =  lvds;
     assign gn[6:3] = ~lvds;
     assign gn[8] = 1;
+
+    /*
+    // unused ?
+    assign gp[2:1] = 0;
+    assign gn[2:1] = 0;
+    assign gp[8] = 0;
+    */
+
+    assign led[7:6] = 0;
+    assign led[5] = adc_convstn;
+    assign led[4] = adc_busy;
+    assign led[3] = adc_reset;
+    assign led[0] = vga_vsync;
+    assign led[1] = vga_hsync;
+    assign led[2] = vga_blank;
 
 endmodule
