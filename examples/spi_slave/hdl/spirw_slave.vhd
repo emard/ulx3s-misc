d5c100df7fd9c50865af1f60b5e1c77cef82896e
emard <vordah@gmail.com>
2020-05-01 09:05:22 +0200

    spirw_slave.vhd wrapper variable number of addr bits

--- examples/spi_slave/hdl/spirw_slave.vhd
+++ examples/spi_slave/hdl/spirw_slave.vhd
@@ -8,6 +8,7 @@ use ieee.std_logic_arith.all;
 entity spirw_slave is
   generic
   (
+    c_addr_bits : natural := 16;
     c_sclk_capable_pin : natural := 0 -- 0-sclk is generic pin, 1-sclk is clock capable pin
   );
   port
@@ -16,7 +17,7 @@ entity spirw_slave is
     csn, sclk, mosi : in    std_logic;
     miso            : inout std_logic;
     rd, wr          : out   std_logic;
-    addr            : out   std_logic_vector(15 downto 0);
+    addr            : out   std_logic_vector(c_addr_bits-1 downto 0);
     data_in         : in    std_logic_vector(7 downto 0);
     data_out        : out   std_logic_vector(7 downto 0)
   );
@@ -26,6 +27,7 @@ architecture syn of spirw_slave is
   component spirw_slave_v -- verilog name and its parameters
   generic
   (
+    c_addr_bits : natural;
     c_sclk_capable_pin : natural
   );
   port
@@ -34,7 +36,7 @@ architecture syn of spirw_slave is
     csn, sclk, mosi : in    std_logic;
     miso            : inout std_logic;
     rd, wr          : out   std_logic;
-    addr            : out   std_logic_vector(15 downto 0);
+    addr            : out   std_logic_vector(c_addr_bits-1 downto 0);
     data_in         : in    std_logic_vector(7 downto 0);
     data_out        : out   std_logic_vector(7 downto 0)
   );
@@ -44,6 +46,7 @@ begin
   spirw_slave_v_inst: spirw_slave_v
   generic map
   (
+    c_addr_bits => c_addr_bits,
     c_sclk_capable_pin => c_sclk_capable_pin
   )
   port map
