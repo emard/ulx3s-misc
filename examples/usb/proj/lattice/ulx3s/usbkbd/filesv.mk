27167b9ba3521804f9d9b4b709555218a07688e4
emard <vordah@gmail.com>
2020-05-06 19:52:08 +0200

    filesv for usbkbd

---
+++ examples/usb/proj/lattice/ulx3s/usbkbd/filesv.mk
@@ -0,0 +1,17 @@
+VERILOG_FILES = $(TOP_MODULE_FILE) \
+  $(CLK0_FILE_NAME) \
+  ../../../../../hex/decoder/hex_decoder_v.v \
+  ../../../../../lcd_st7789/hdl/st7789_video_verilog/lcd_video.v \
+  ../../../../usbhost/usbh_sie.v \
+  ../../../../usbhost/usbh_crc5.v \
+  ../../../../usbhost/usbh_crc16.v \
+  ../../../../usbhost/usbh_host_hid.v \
+
+# convertible with vhd2vl
+VHDL_FILES = \
+  ../../../../usb11_phy_vhdl/usb_phy.vhd \
+  ../../../../usb11_phy_vhdl/usb_rx_phy.vhd \
+  ../../../../usb11_phy_vhdl/usb_tx_phy.vhd \
+  ../../../../../dvi/hdl/vga.vhd \
+  ../../../../../dvi/hdl/vga2dvid.vhd \
+  ../../../../../dvi/hdl/tmds_encoder.vhd \
