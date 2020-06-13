20ce3d4ca9a552f94e980b2a8c0a7eba8a3bcfed
emard <vordah@gmail.com>
2020-06-13 14:25:17 +0200

    ecp5pll.vhd: simplify

--- examples/ecp5pll/hdl/vhd/ecp5pll.vhd
+++ examples/ecp5pll/hdl/vhd/ecp5pll.vhd
@@ -181,10 +181,9 @@ architecture mix of ecp5pll is
             error := abs(fout-out0_hz);
             for channel in 1 to 3 loop
               if sfreq(channel) > 0 then
+                div := 1;
                 if fvco >= sfreq(channel) then
                   div := fvco/sfreq(channel);
-                else
-                  div := 1;
                 end if;
                 freq  := fvco/div;
                 error := error + abs(freq-sfreq(channel));
@@ -215,7 +214,10 @@ architecture mix of ecp5pll is
       -- generate secondary outputs
       for channel in 1 to 3 loop
         if sfreq(channel) > 0 then
-          div  := params.fvco/sfreq(channel);
+          div := 1;
+          if params.fvco >= sfreq(channel) then
+            div := params.fvco/sfreq(channel);
+          end if;
           freq := params.fvco/div;
           phase_compensation := div*8-8;
           phase_count_x8 := phase_compensation + 8*div*sphase(channel)/360;
