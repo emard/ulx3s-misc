4ca3851be24a1718b48b505f7a86499c13031875
emard <vordah@gmail.com>
2020-08-06 22:34:10 +0200

    rtc e-ink with graphics clock

--- examples/rtc/micropython-mcp7940n/esp32/rtceink.py
+++ examples/rtc/micropython-mcp7940n/esp32/rtceink.py
@@ -1,6 +1,7 @@
 from time import localtime,sleep_ms
 from random import randint
 import framebuf
+from math import sin,cos,pi
 from machine import Pin, I2C
 from ntptime import settime
 import mcp7940
@@ -25,20 +26,37 @@ else:
 
 ntp=0
 
+def graphic(t,color):
+  x0=epd.width//2
+  y0=x0
+  rhour=epd.width*4//16
+  rmin=epd.width*6//16
+  rmax=epd.width//2
+  rtick=epd.width*7//16
+  # draw ticks
+  for i in range(12):
+    ahour=i*pi/6
+    fb.line(x0+int(rtick*sin(ahour)),y0-int(rtick*cos(ahour)), x0+int(rmax*sin(ahour)),y0-int(rmax*cos(ahour)), color)
+  # draw hands
+  ahour=(t[3]+t[4]/60)*pi/6
+  amin=t[4]*pi/30
+  fb.line(x0,y0, x0+int(rhour*sin(ahour)),y0-int(rhour*cos(ahour)), color)
+  for i in range(3):
+    fb.line(x0,y0, x0+int(rmin*sin(amin)),y0-int(rmin*cos(amin)), color)
+    amin-=pi/720
+
 def disp(t=None,color=0):
   fb.fill(0xFF)
-  fb.text("Micropython!", 0,0, 0)
-  fb.hline(0,10, 96, 0)
-  #fb.line(0,10, 96,106, 0)
   if t:
     td=t
   else:
     td=mcp.time
   weekday=["MO","TU","WE","TH","FR","SA", "SU"]
   source=["MCP","NTP"]
-  time_str="%04d-%02d-%02d %02d:%02d:%02d %02s %03s" % \
-    (td[0],td[1],td[2],td[3],td[4],td[5],weekday[td[6]],source[ntp])
-  fb.text(time_str, 0,40+(10*td[4])%120, color)
+  time_str="%04d-%02d-%02d %02s %02d:%02d:%02d %03s" % \
+    (td[0],td[1],td[2], weekday[td[6]], td[3],td[4],td[5], source[ntp])
+  fb.text(time_str, 0,42+(10*td[4])%120, color)
+  graphic(td,color)
   epd.display_frame(frame)
 
 def shutdown():
@@ -55,12 +73,12 @@ def clock():
   fb.fill(0xFF)
   epd.write_frame(frame,IL382x.WRITE_RAM_RED) # must be to fade
   epd.write_frame(frame,IL382x.WRITE_RAM)
-  epd.refresh_frame()
   td=mcp.time
-  # wait for full minute
+  # wait for next minute
   while td[5]!=0:
     sleep_ms(500)
     td=mcp.time
+  epd.refresh_frame()
   disp(td,0)
   epd.sleep()
   mcp.battery_backup_enable(1)
