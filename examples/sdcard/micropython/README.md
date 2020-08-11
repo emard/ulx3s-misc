bd801759418e552b0bdd39cb1d80b3d169cefb53
emard <vordah@gmail.com>
2020-08-11 11:12:50 +0200

    sd card test improved with longer buffer and sector mode

--- examples/sdcard/micropython/README.md
+++ examples/sdcard/micropython/README.md
@@ -20,6 +20,16 @@ in esp32 directory there is small test bench for reading speed,
 create some 1-10 MB "long_file.bin" on root of SD card, test will
 read it:
 
+1-bit mode SDCard(slot=3):
+
     >>> import sdtest
     ['long_file.bin']
-    1055 KB in 866 ms => 1218 KB/s
+    1056 KB in 752 ms => 1404 KB/s file read
+    4096 KB in 2746 ms => 1491 KB/s raw sector read
+
+4-bit mode SDCard():
+
+    >>> import sdtest
+    ['long_file.bin']
+    1056 KB in 492 ms => 2146 KB/s file read
+    4096 KB in 1777 ms => 2305 KB/s raw sector read
--- examples/sdcard/micropython/esp32/sdtest.py
+++ examples/sdcard/micropython/esp32/sdtest.py
@@ -2,19 +2,27 @@ from machine import SDCard
 from os import mount,umount,listdir
 from time import ticks_ms
 
-def mnt():
-  mount(SDCard(),"/sd") # 4-bit mode
-  #mount(SDCard(slot=3),"/sd") # 1-bit mode
+def run():
+  #sd=SDCard() # 4-bit mode
+  sd=SDCard(slot=3) # 1-bit mode
+  mount(sd,"/sd")
   print(listdir("/sd"))
   f=open("/sd/long_file.bin","rb") # any 1-10 MB long file
-  b=bytearray(1024)
+  b=bytearray(16384)
   i=0
   t1=ticks_ms()
   while f.readinto(b):
     i+=1
   t2=ticks_ms()
-  print("%d KB in %d ms => %d KB/s" % (i,t2-t1,1000*i//(t2-t1)))
+  print("%d KB in %d ms => %d KB/s file read" % (i*len(b)//1024,t2-t1,1000*i*len(b)//1024//(t2-t1)))
   f.close()
   umount("/sd")
-
-mnt()
+  i=0
+  t1=ticks_ms()
+  while i<256: 
+    sd.readblocks(i,b)
+    i+=1
+  t2=ticks_ms()
+  print("%d KB in %d ms => %d KB/s raw sector read" % (i*len(b)//1024,t2-t1,1000*i*len(b)//1024//(t2-t1)))
+  sd.deinit()
+run()
