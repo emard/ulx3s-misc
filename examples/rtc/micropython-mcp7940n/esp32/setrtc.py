f49246b5eff5dbb2c28141f5b382ab73c1eba2a1
emard <vordah@gmail.com>
2020-08-24 08:38:13 +0200

    setrtc.py trim -40ppm

--- examples/rtc/micropython-mcp7940n/esp32/setrtc.py
+++ examples/rtc/micropython-mcp7940n/esp32/setrtc.py
@@ -12,7 +12,7 @@ i2c = I2C(sda=Pin(16), scl=Pin(17), freq=400000)
 mcp = mcp7940.MCP7940(i2c)
 
 mcp.control=0
-mcp.trim=-37
+mcp.trim=-40
 #mcp.battery=1
 #print("battery %s" % ("enabled" if mcp.battery else "disabled"))
 print("trim %+d ppm" % mcp.trim)
