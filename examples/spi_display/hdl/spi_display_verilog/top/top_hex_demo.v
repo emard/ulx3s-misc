3675a836e3007b77655ff06a18759ae55321cd23
emard <vordah@gmail.com>
2021-01-24 18:54:17 +0100

    st7789 hex demo follow dual clock lcd_video

--- examples/spi_display/hdl/spi_display_verilog/top/top_hex_demo.v
+++ examples/spi_display/hdl/spi_display_verilog/top/top_hex_demo.v
@@ -76,7 +76,7 @@ module top_hex_demo
   wire w_oled_csn;
   lcd_video
   #(
-    .c_clk_mhz(125),
+    .c_clk_spi_mhz(125),
     .c_init_file("st7789_linit_xflip.mem"),
     .c_clk_phase(0),
     .c_clk_polarity(1),
@@ -84,8 +84,9 @@ module top_hex_demo
   )
   lcd_video_inst
   (
-    .clk(clk),
     .reset(~btn[0]),
+    .clk_pixel(clk),
+    .clk_spi(clk),
     .x(x),
     .y(y),
     .next_pixel(next_pixel),
