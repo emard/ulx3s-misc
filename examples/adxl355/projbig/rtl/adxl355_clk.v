7eb75d44e125a400687914210d120e72b5fd1865
emard <vordah@gmail.com>
2021-02-09 01:14:06 +0100

    adxl355 simplify and fix sync signal generation

--- examples/adxl355/projbig/rtl/adxl355_clk.v
+++ examples/adxl355/projbig/rtl/adxl355_clk.v
@@ -3,59 +3,57 @@
 `default_nettype none
 module adxl355_clk
 #(
-  clk_out0_hz  = 40*1000000, // Hz, 40 MHz, PLL internal clock divisible by 512 to make 2*clk_adxl_hz=2048 kHz
-  clk_pps_hz   = 1,          // Hz, 1 Hz, TODO not used
-  clk_adxl_hz  = 1024*1000,  // Hz, 1024 kHz, LUT-generated adxl clock
+  clk_out0_hz  = 40*1000000, // Hz, 40 MHz, PLL internal clock
+  pps_hz       = 1,          // Hz, 1 Hz
+  pps_tol_us   = 500,        // us, 500 us, PPS input +-tolerance
   clk_sync_hz  = 1000,       // Hz, 1 kHz SYNC clock, sample rate
-  clk_sync_pulse_bits = 5,   // 2**(n-1)/2048kHz pulse width of SYNC clock minimal: 4->3.91us, reliable: 5->7.81us
-  pa_sync_bits = 24          // SYNC phase accumulator bits
+  clk_sync_pulse_bits = 9,   // 2**(n-1)/clk_out0_hz s pulse width of SYNC clock minimal: 3.91us, reliable: 9->6.4us
+  pa_sync_bits = 32          // SYNC phase accumulator bits
 )
 (
-  input  i_clk,      // 40 kHz system clock
-  input  i_pps,      // 1 Hz pulse per second from GPS TODO not used
-  output o_clk_adxl, // 1024 kHz
-  output o_clk_sync  // 1 kHz
+  input  i_clk,       // 40 kHz system clock
+  input  i_pps,       // 1 Hz pulse per second from GPS TODO not used
+  output o_pps_valid, // 1 when pps in +-clk_pps_tol_us
+  output o_clk_sync   // 1 kHz
 );
-  localparam divider1024kHz=clk_out0_hz*2/clk_adxl_hz; // default 78125 for half-period 2048 kHz from 40 MHz
-  reg clk_sync; // 1.024kHz, must become 1 cca 25ns (1 clk cycle) before clk_adxl and keep 1 for at least 4 clk cycles
-  reg clk_adxl; // 1024 kHz, toggle reg
-  reg [$clog2(divider1024kHz)-1:0] clk_adxl_cnt; // divider counter for 1024 kHz
+  localparam real re_pa_inc = 1.0 * clk_sync_hz * 2.0 * 2**(pa_sync_bits-1) / clk_out0_hz;
+  localparam integer int_pa_inc = re_pa_inc; // 107374 for 1kHz
+  reg [pa_sync_bits-1:0] reg_pa_inc = int_pa_inc; // sample rate frequency fine adjust reg implicit real->integer
   reg [pa_sync_bits-1:0] pa_sync; // phase accumulator for sync signal
-  localparam real re_pa_inc = 2.0 * clk_sync_hz * 2**pa_sync_bits / clk_adxl_hz; // default 32768 for 1kHz sample rate
-  reg [pa_sync_bits-1:0] int_pa_inc = re_pa_inc; // sample rate frequency fine adjust reg implicit real->integer
-  wire [pa_sync_bits:0] pa_sync_next = pa_sync + int_pa_inc; // one bit more for carry
-  reg [clk_sync_pulse_bits-1:0] cnt_sync_fall;
-  wire clk2048kHz = clk_adxl_cnt == divider1024kHz-1 ? 1 : 0;
-  localparam clk_25ns_bits = 2;
-  reg [clk_25ns_bits-1:0] clk_adxl_25ns; // 25ns step delay shift register
-  reg [clk_25ns_bits-1:0] clk_sync_25ns; // 25ns step delay shift register
+  wire [pa_sync_bits-1:0] pa_sync_next = pa_sync + reg_pa_inc; // one bit more for carry
+  always @(posedge i_clk)
+    pa_sync <= pa_sync_next;
+  assign o_clk_sync = pa_sync[pa_sync_bits-1]; // 1 kHz
+
+  // check PPS for tolerance
+  localparam real re_pps_min_cnt = 1.0 * clk_out0_hz/pps_hz - 1.0 * pps_tol_us*clk_out0_hz/1000000;
+  localparam real re_pps_max_cnt = 1.0 * clk_out0_hz/pps_hz + 1.0 * pps_tol_us*clk_out0_hz/1000000;
+  localparam integer pps_min_cnt = re_pps_min_cnt;
+  localparam integer pps_max_cnt = re_pps_max_cnt;
+  localparam cnt_pps_bits = 32;
+  reg [cnt_pps_bits-1:0] cnt_pps, cnt_pps_prev;
+  reg [1:0] pps_shift; // for PPS edge detection
+  reg pps_valid_tmp, pps_valid;
   always @(posedge i_clk)
   begin
-    if(clk2048kHz) 
-    begin // 2048 kHz execution
-      clk_adxl_cnt <= 0;
-      clk_adxl <= ~clk_adxl; // 1024 kHz toggle
-      pa_sync <= pa_sync_next; // changes value 50 ns before clk_adxl edge
-      if(pa_sync_next[pa_sync_bits]) // carry set
-      begin // 1 kHz execution
-        clk_sync <= 1; // changes value 25 ns before clk_adxl_cnt, required setup time
-        cnt_sync_fall <= 0; // counts up to MSB set
-      end
-      else // counter to delay turn off
-      begin
-        if(cnt_sync_fall[clk_sync_pulse_bits-1]) // MSB set, minimum pulse width should be 4x 1024kHz cycles = 3.91us, count 8 should be 2x more than required
-          clk_sync <= 0; // turn off clk_sync
-        else
-          cnt_sync_fall <= cnt_sync_fall+1;
-      end
+    pps_shift <= {i_pps, pps_shift[1]};
+    if(pps_shift == 2'b10) // rising edge of PPS
+    begin
+      pps_valid <= pps_valid_tmp;
+      cnt_pps_prev <= cnt_pps;
+      cnt_pps <= 0;
     end
     else
-      clk_adxl_cnt <= clk_adxl_cnt+1;
-    clk_adxl_25ns <= { clk_adxl_25ns[clk_25ns_bits-2:0], clk_adxl };
-    clk_sync_25ns <= { clk_sync_25ns[clk_25ns_bits-2:0], clk_sync };
-  end
-  assign o_clk_adxl = clk_adxl_25ns[1]; // 1024 kHz, n*25ns delayed
-  assign o_clk_sync = clk_sync_25ns[0]; // 1 kHz, n*25ns delayed
+      cnt_pps <= cnt_pps + 1;
 
+    if(cnt_pps == pps_min_cnt)
+      pps_valid_tmp <= 1;
+    else
+    begin
+      if(cnt_pps == pps_max_cnt || cnt_pps == 0)
+        pps_valid_tmp <= 0;
+    end
+  end
+  assign o_pps_valid = pps_valid;
 endmodule
 `default_nettype wire
--- examples/adxl355/projbig/top/top_adxl355log.v
+++ examples/adxl355/projbig/top/top_adxl355log.v
@@ -1,7 +1,7 @@
 // passthru for ADXL355 accelerometer
 // use with:
 // adxl355.filter(0)
-// adxl355.sync(6)
+// adxl355.sync(2) # DRDY=SYNC input, internal oscillator
 // adxl355.multird16()
 
 /*
@@ -25,11 +25,8 @@ Pin 12      Digital Power        VDD
 `default_nettype none
 module top_adxl355log
 #(
-  clk_out0_hz  = 40*1000000, // Hz, 40 MHz, PLL internal clock divisible by 512 to make 2*clk_adxl_hz=2048 kHz
-  clk_adxl_hz  = 1024*1000,  // Hz, 1024 kHz, LUT-generated adxl clock
-  clk_sync_hz  = 1000,       // Hz, 1 kHz SYNC clock, sample rate
-  clk_sync_pulse_bits = 5,   // 2**(n-1)/2048kHz pulse width of SYNC clock minimal: 4->3.91us, reliable: 5->7.81us
-  pa_sync_bits = 24          // SYNC phase accumulator bits
+  clk_out0_hz  = 40*1000000, // Hz, 40 MHz, PLL generated internal clock
+  clk_sync_hz  = 1000        // Hz, 1 kHz SYNC pulse, sample rate
 )
 (
   input         clk_25mhz,
@@ -38,7 +35,8 @@ module top_adxl355log
   output        oled_csn, // LCD ST7789 backlight off
   //inout  [27:0] gp,gn,
   output        gp13, // ESP32   MISO
-  output        gp14,gp15, // ADXL355 DRDY,INT2
+  output        gp14, // ADXL355 DRDY
+  input         gp15, // ADXL355 INT2
   input         gp17, // ADXL355 INT1
   input         gn15, // ADXL355 MISO
   output        gn14,gn16,gn17, // ADXL355 SCLK,MOSI,CSn
@@ -52,9 +50,8 @@ module top_adxl355log
   assign ftdi_rxd = wifi_txd;
 
   wire int1 = gp17;
-  wire int2; // gp15;
+  wire int2 = gp15;
   wire drdy; // gp14;
-  assign gp15 = int2;
   assign gp14 = drdy;
 
   wire csn, mosi, miso, sclk;
@@ -85,26 +82,47 @@ module top_adxl355log
   );
   wire clk = clocks[0]; // 40 MHz system clock
 
+  // generate PPS signal (1 Hz, 100 ms duty cycle)
+  localparam pps_cnt_max = clk_out0_hz; // +-20 kHz tolerance
+  localparam pps_width   = pps_cnt_max/10;   
+  reg [$clog2(pps_cnt_max)-1:0] pps_cnt;
+  reg pps;
+  always @(posedge clk)
+  begin
+    if(pps_cnt == pps_cnt_max-1)
+      pps_cnt <= 0;
+    else
+      pps_cnt <= pps_cnt+1;
+    if(pps_cnt == 0)
+      pps <= 1;
+    else if(pps_cnt == pps_width-1)
+      pps <= 0;
+  end
+
+  wire pps_valid;
   adxl355_clk
   #(
-    .clk_out0_hz(40*1000000), // Hz, 40 MHz, PLL internal clock divisible by 512 to make 2*clk_adxl_hz=2048 kHz
-    .clk_pps_hz(1),           // Hz, 1 Hz, TODO not used
-    .clk_adxl_hz(1024*1000),  // Hz, 1024 kHz, LUT-generated adxl clock
-    .clk_sync_hz(1000)        // Hz, 1 kHz SYNC clock, sample rate
+    .clk_out0_hz(clk_out0_hz), // Hz, 40 MHz, PLL internal clock
+    .pps_hz(1),                // Hz, 1 Hz
+    .pps_tol_us(500),          // us, 500 us, default +- tolerance for pulse rising edge
+    .clk_sync_hz(clk_sync_hz)  // Hz, 1 kHz SYNC clock, sample rate
   )
   adxl355_clk_inst
   (
     .i_clk(clk),
-    .i_pps(0),
-    .o_clk_adxl(int2),
+    .i_pps(pps), // rising edge sensitive
+    .o_pps_valid(pps_valid),
     .o_clk_sync(drdy)
   );
 
   // LED monitoring
   assign led[7:4] = {drdy,int2,int1,1'b0};
   //assign led[3:0] = {gn27,gn26,gn25,gn24};
-  assign led[3:0] = {sclk,gp13,mosi,csn};
+  //assign led[3:0] = {sclk,gp13,mosi,csn};
   //assign led[3:0] = {sclk,miso,mosi,csn};
+  assign led[3:2] = 0;
+  assign led[1] = pps_valid;
+  assign led[0] = pps;
   
   assign oled_csn = 0; // st7789 backlight off
   
