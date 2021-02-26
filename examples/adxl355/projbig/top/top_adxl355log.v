6317404fe7f8ad5a518f038519339a8021a5b4fe
emard <vordah@gmail.com>
2021-02-27 00:27:00 +0100

    adxl355 add esp32 passthru to allow program esp32 using the core

--- examples/adxl355/projbig/top/top_adxl355log.v
+++ examples/adxl355/projbig/top/top_adxl355log.v
@@ -25,6 +25,7 @@ Pin 12      Digital Power        VDD
 `default_nettype none
 module top_adxl355log
 #(
+  C_prog_release_timeout = 26, // esp32 programming default n=26, 2^n / 25MHz = 2.6s
   clk_out0_hz  = 40*1000000, // Hz, 40 MHz, PLL generated internal clock
   pps_n        = 10,         // N, 1 Hz, number of PPS pulses per interval
   pps_s        = 1,          // s, 1 s, PPS interval
@@ -37,7 +38,6 @@ module top_adxl355log
   output  [7:0] led,
   output        oled_csn, // LCD ST7789 backlight off
   output  [3:0] audio_l, audio_r,
-  //inout  [27:0] gp,gn,
   output        gp13, // ESP32   MISO
   output        gp14, // ADXL355 DRDY
   input         gp15, // ADXL355 INT2
@@ -53,18 +53,51 @@ module top_adxl355log
   output        wifi_en,
   output        wifi_rxd,
   input         wifi_txd,
-  input         wifi_gpio5,
   output        wifi_gpio0, 
-  input         wifi_gpio16, wifi_gpio17
+  input         wifi_gpio5,
+  input         wifi_gpio16, wifi_gpio17,
+  inout   [3:0] sd_d, // wifi_gpio 13,12,4,2
+  input         sd_cmd, sd_clk,
+  output        sd_wp // BGA pin exists but not connected on PCB
 );
-  // usb-serial console
-  assign wifi_rxd = ftdi_txd;
+  // TX/RX passthru
   assign ftdi_rxd = wifi_txd;
-  // programming esp32
-  assign wifi_en    = ~ftdi_ndtr |  ftdi_nrts;
-  assign wifi_gpio0 =  ftdi_ndtr | ~ftdi_nrts;
-  // todo make wifi_gpio0 bidirectional when not programming
-  // so esp32-spi -> fpga-adxl is possible
+  assign wifi_rxd = ftdi_txd;
+
+  // Programming logic
+  // SERIAL  ->  ESP32
+  // DTR RTS -> EN IO0
+  //  1   1     1   1
+  //  0   0     1   1
+  //  1   0     0   1
+  //  0   1     1   0
+  
+  reg  [1:0] R_prog_in;
+  wire [1:0] S_prog_in  = { ftdi_ndtr, ftdi_nrts };
+  wire [1:0] S_prog_out = S_prog_in == 2'b10 ? 2'b01 
+                        : S_prog_in == 2'b01 ? 2'b10 : 2'b11;
+  assign wifi_en = S_prog_out[1];
+  assign wifi_gpio0 = S_prog_out[0] & btn[0]; // holding BTN0 will hold gpio0 LOW, signal for ESP32 to take control
+
+  //assign wifi_en = S_prog_out[1] & btn[0]; // holding BTN0 disables ESP32, releasing BTN0 reboots ESP32
+  //assign wifi_gpio0 = S_prog_out[0];
+
+  // detecting programming ESP32 and reset timeout
+  reg [C_prog_release_timeout:0] R_prog_release;
+  always @(posedge clk_25mhz)
+  begin
+    R_prog_in <= S_prog_in;
+    if(S_prog_out == 2'b01 && R_prog_in == 2'b11)
+      R_prog_release <= 0; // keep resetting during ESP32 programming
+    else
+      if(R_prog_release[C_prog_release_timeout] == 1'b0)
+        R_prog_release <= R_prog_release + 1; // increment until MSB=0
+  end
+  // wifi_gpio2 for programming must go together with wifi_gpio0
+  // wifi_gpio12 (must be 0 for esp32 fuse unprogrammed)
+  assign sd_d  = R_prog_release[C_prog_release_timeout] ? 4'hz : { 3'b101, S_prog_out[0] }; // wifi_gpio 13,12,4,2
+  assign sd_wp = sd_clk | sd_cmd | sd_d; // force pullup for 4'hz above for listed inputs to make SD MMC mode work
+  // sd_wp is not connected on PCB, just to prevent optimizer from removing pullups
 
   wire int1 = gp17;
   wire int2 = gp15;
