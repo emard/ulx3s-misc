db01db23184a6fdf857fa6ac4ff6b8a8d3bb43fa
emard <vordah@gmail.com>
2021-04-03 11:48:35 +0200

    rds toplevel instantiate verilog ram

--- examples/fm/top/top_fm.v
+++ examples/fm/top/top_fm.v
@@ -32,6 +32,13 @@ module top_fm
   always @(posedge clk)
     beep <= beep+1;
 
+  reg [7:0] rds_ram[0:51];
+  initial
+    $readmemh("message_ps.mem", rds_ram);
+  wire [5:0] rds_addr;
+  reg  [7:0] rds_data;
+  always @(posedge clk)
+    rds_data <= rds_ram[rds_addr];
   fmgen_test
   //#(
   //  .C_fmdds_hz(250*1000000),
@@ -42,12 +49,15 @@ module top_fm
   (
     .clk(clk),
     .clk_fmdds(clk_fmdds),
-    .pcm_in_left(0), // beep, may spoil RDS
-    .pcm_in_right(0), // beep, may spoil RDS
+    .pcm_in_left(btn[1] ? beep[15:1] : 0), // beep, may spoil RDS
+    .pcm_in_right(btn[2] ? beep[15:1] : 0), // beep, may spoil RDS
     .cw_freq(107900000),
-    .led(led),
+    .rds_addr(rds_addr),
+    .rds_data(rds_data),
     .fm_antenna(ant_433mhz)
   );
+
+  assign led = rds_data;
 
 endmodule
 `default_nettype wire
