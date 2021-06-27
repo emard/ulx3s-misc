14458d75690685302fa4c9a65faf3c17bcc731bf
Emard <vordah@gmail.com>
2021-06-27 14:37:59 +0200

    extend coefficients type with variable area, initializing them 0

--- fpga/ulx3s/calc/hdl/coefficients_pack.vhd
+++ fpga/ulx3s/calc/hdl/coefficients_pack.vhd
@@ -6,7 +6,7 @@ use ieee.numeric_std.all;
 use ieee.math_real.all; -- for real numbers
 
 package coefficients is
-type coefficients_type is array(0 to 19) of real;
+type coefficients_type is array(0 to 16+4+6*4-1) of real;
 constant coefficients_250mm_matrix: coefficients_type := (
 -- ST matrix 4x4
   0.9966071  ,  1.091514e-2, -2.083274e-3,  3.190145e-4  , -- 0*4+
@@ -15,6 +15,6 @@ constant coefficients_250mm_matrix: coefficients_type := (
   3.335013   ,  0.3376467  ,-39.12762    ,  0.4347564    , -- 3*4+
 -- PR matrix 1x4
   5.476107e-3,  1.388776   ,  0.2275968  , 35.79262      , -- 4*4+
-others => 0.0 -- should not be needed
+others => 0.0 -- 5*4+ .. 10*4+ variable area
 );
 end coefficients;
