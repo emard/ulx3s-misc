75c667362e05eae459bce91deb2f1716d4ec2500
Emard <vordah@gmail.com>
2021-06-27 19:24:37 +0200

    some early debug attempt to read and write data in matrix bram

--- fpga/ulx3s/calc/hdl/calc.vhd
+++ fpga/ulx3s/calc/hdl/calc.vhd
@@ -18,6 +18,7 @@ generic (
 );
 port (
   clk: in std_logic;
+  start: in std_logic;
   d1: in  std_logic_vector(31 downto 0);
   d0: out std_logic_vector(31 downto 0)
 );
@@ -26,7 +27,7 @@ end;
 architecture RTL of calc is
   constant c_b: integer := 0;
   constant int_scale_matrix_2n: integer := 20; -- approx 1e6
-  type int32_coefficients_type is array(0 to 19) of signed(31 downto 0);
+  type int32_coefficients_type is array(0 to 17*4-1) of signed(31 downto 0);
   -- function to scale and convert real matrix to integers
   function matrix_real2int(x: coefficients_type; scale: integer)
     return int32_coefficients_type is
@@ -34,14 +35,97 @@ architecture RTL of calc is
       variable    y: int32_coefficients_type;
     begin
       for i in 0 to 3 loop
-        for j in 0 to 4 loop
+        for j in 0 to 16 loop
           y(i+4*j) := to_signed(integer(x(i+4*j)*real(scale)),32);
         end loop;
       end loop;
     return y;
   end matrix_real2int;
-  constant int32_coefficients_matrix: int32_coefficients_type := 
+  signal int32_coefficients_matrix: int32_coefficients_type := 
     matrix_real2int(coefficients_250mm_matrix, 2**int_scale_matrix_2n);
+  signal a,b,c: signed(31 downto 0);
+  signal ab: signed(63 downto 0);
+  signal result: signed(31 downto 0);
+  signal reset_c, calc_c: std_logic;
+  signal cnt: unsigned(3 downto 0);
+  signal ia, ib: unsigned(6 downto 0); -- indexes for matrix
+  signal matrix_read, matrix_write: std_logic := '0';
 begin
-  d0 <= std_logic_vector(int32_coefficients_matrix(to_integer(unsigned(d1))));
+  --d0 <= std_logic_vector(int32_coefficients_matrix(to_integer(unsigned(d1))));
+  --d0 <= std_logic_vector(bc(31 downto 0));
+  d0 <= std_logic_vector(result);
+  
+  -- indexes for data fetch, BRAM
+  process(clk)
+  begin
+    if rising_edge(clk) then
+      if matrix_read = '1' then
+        a <= int32_coefficients_matrix(to_integer(ia));
+        b <= int32_coefficients_matrix(to_integer(ib));
+      end if;
+      if matrix_write = '1' then
+        int32_coefficients_matrix(to_integer(ia)) <= c;
+      end if;
+    end if;
+  end process;
+
+  -- sum of scaled integer multiplication
+  ab <= a*b;
+  process(clk)
+  begin
+    if rising_edge(clk) then
+      if reset_c = '1' then
+        c <= (others => '0');
+      else
+        if calc_c = '1' then
+          c <= c+ab(int_scale_matrix_2n+31 downto int_scale_matrix_2n);
+        end if;
+      end if;
+    end if;
+  end process;
+  
+  --b <= x"00200000"; -- 2.0 scaled fixed point 20-bit
+  --c <= x"00000030";
+  
+  process(clk)
+  begin
+    if rising_edge(clk) then
+      if start = '1' then
+        cnt <= (others => '0');
+      else
+        if cnt = x"0" then
+          reset_c <= '1';
+          ia <= to_unsigned(0, 7);
+          ib <= to_unsigned(1, 7);
+        else
+          reset_c <= '0';
+          if cnt = x"5" then
+            ia <= to_unsigned(5*4, 7);
+          end if;
+        end if;
+        if cnt = x"1" then
+          matrix_read <= '1';
+        else
+          matrix_read <= '0';
+        end if;
+        if cnt = x"2" then
+          calc_c <= '1';
+        else
+          calc_c <= '0';
+        end if;
+        if cnt = x"4" then
+          result <= c;
+        end if;
+        if cnt = x"6" then
+          matrix_write <= '1';
+        else
+          matrix_write <= '0';
+        end if;
+        if cnt(3) = '0' then
+          cnt <= cnt + 1;
+        end if;
+      end if;
+    end if;
+  end process;
+  
 end;
--- fpga/ulx3s/proj/top/top_adxl355log.v
+++ fpga/ulx3s/proj/top/top_adxl355log.v
@@ -608,6 +608,7 @@ module top_adxl355log
   calc_inst
   (
     .clk(clk),
+    .start(btn_rising[1]),
     .d1(ma),
     .d0(data[31:0])
   );
