25c9bf5fc36a34b734553ed01d661f6d0cb0b958
Emard <vordah@gmail.com>
2021-06-27 11:12:46 +0200

    function to scale and convert matrix

--- fpga/ulx3s/calc/hdl/calc.vhd
+++ fpga/ulx3s/calc/hdl/calc.vhd
@@ -24,6 +24,25 @@ end;
 
 architecture RTL of calc is
   constant c_b: integer := 0;
+  constant int_scale_matrix_2n: integer := 20; -- approx 1e6
+  type uint32_coefficients_type is array(0 to 19) of unsigned(31 downto 0);
+  -- function to scale and convert real matrix to integers
+  function matrix_real2int(x: coefficients_type; scale: integer)
+    return uint32_coefficients_type is
+      variable i,j: integer;
+      variable y: uint32_coefficients_type;
+    begin
+      for i in 0 to 3 loop
+        for j in 0 to 4 loop
+          y(i+4*j) := to_unsigned(integer(x(i+4*j)*real(scale)),32);
+        end loop;
+      end loop;
+    return y;
+  end matrix_real2int;
+  constant uint32_coefficients_matrix: uint32_coefficients_type := 
+    matrix_real2int(coefficients_matrix, 2**int_scale_matrix_2n);
 begin
-  d0 <= d1;
+  --d0 <= d1;
+  --d0 <= std_logic_vector(a00);
+  d0 <= std_logic_vector(uint32_coefficients_matrix(4*4+0));
 end;
--- fpga/ulx3s/calc/hdl/coefficients_pack.vhd
+++ fpga/ulx3s/calc/hdl/coefficients_pack.vhd
@@ -6,9 +6,15 @@ use ieee.numeric_std.all;
 use ieee.math_real.all; -- for real numbers
 
 package coefficients is
-type coefficients_type is array(0 to 3) of real;
+type coefficients_type is array(0 to 19) of real;
 constant coefficients_matrix: coefficients_type := (
-1.0, 2.0, 3.0, 4.0,
-others => 0.0
+-- ST matrix 4x4
+1.1, 2.1, 3.1, 4.1,
+1.2, 2.2, 3.2, 4.2,
+1.3, 2.3, 3.3, 4.3,
+1.4, 2.4, 3.4, 4.4,
+-- PR matrix 1x4
+1.5, 2.5, 3.5, 4.5,
+others => 0.0 -- should not be needed
 );
 end coefficients;
