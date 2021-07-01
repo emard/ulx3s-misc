bc2ac132b1c206e3101c6a79b9558604d7984ed2
Emard <vordah@gmail.com>
2021-07-01 20:30:23 +0200

    adjust acceleration to prevent slope build up DC it oscillates, so maybe FIXME

--- fpga/ulx3s/calc/hdl/slope.vhd
+++ fpga/ulx3s/calc/hdl/slope.vhd
@@ -5,6 +5,11 @@
 -- from acceleration and speed calculate slope
 -- calculate slope at every interval_mm
 
+-- slope should not build up much DC
+-- so feedback loop that adjusts acceleration +-1
+-- each step
+-- TODO better (faster) DC removal
+
 library ieee;
 use ieee.std_logic_1164.all;
 -- use ieee.std_logic_arith.all; -- replaced by ieee.numeric_std.all
@@ -41,28 +46,68 @@ end;
 architecture RTL of slope is
   signal ix, ix_next, ix_inc: signed(31 downto 0); -- traveled distance um
   signal sl, sr, sr_next, sl_next : signed(41 downto 0); -- sum of const/vz^2, 42 bits (last 10 bits dropped at output)
-  signal avz2l, avz2r: signed(47 downto 0);
+  signal avz2l, avz2r: signed(47 downto 0); -- multiplier
   signal iazl, iazr : signed(15 downto 0); -- z-acceleration signed
+  signal adifl, adifr : signed(15 downto 0); -- z-acceleration differential adjust
   signal icvx2: signed(31 downto 0);
   signal next_interval : std_logic;
   constant interval_x : signed(31 downto 0) := to_signed(1000*interval_mm,32); -- interval um
 begin
   ix_inc <= signed(x_inc);
-  iazl   <= signed(azl);
-  iazr   <= signed(azr);
+  process(clk)
+  begin
+    if rising_edge(clk) then
+      if enter = '1' then
+      --if next_interval = '1' then
+        -- slowly adjust acceleration to prevent slope build up DC
+        if sl < 0 then
+          if avz2l < 0 then
+            adifl <= adifl + 2;
+          else
+            adifl <= adifl + 1;
+          end if;
+        else
+          if sl > 0 then
+            if avz2l > 0 then
+              adifl <= adifl - 2;
+            else
+              adifl <= adifl - 1;
+            end if;
+          end if;
+        end if;
+        if sr < 0 then
+          if avz2r < 0 then
+            adifr <= adifr + 2;
+          else
+            adifr <= adifr + 1;
+          end if;
+        else
+          if sr > 0 then
+            if avz2r > 0 then
+              adifr <= adifr - 2;
+            else
+              adifr <= adifr - 1;
+            end if;
+          end if;
+        end if;
+      end if;
+      iazl <= adifl + signed(azl);
+      iazr <= adifr + signed(azr);      
+    end if;
+  end process;
 
   icvx2  <= signed(cvx2);
 
   -- x_inc should be less tnan interval_x
   ix_next  <= ix + ix_inc;
-  avz2l    <= iazl * icvx2;
-  avz2r    <= iazr * icvx2;
   sl_next  <= sl + avz2l(41 downto 0);
   sr_next  <= sr + avz2r(41 downto 0);
 
   process(clk)
   begin
     if rising_edge(clk) then
+      avz2l <= iazl * icvx2; -- differential of slope
+      avz2r <= iazr * icvx2; -- differential of slope
       if reset = '1' then
         ix <= (others => '0');
         sl <= (others => '0');
@@ -78,6 +123,8 @@ begin
           end if;
           sl <= sl_next;
           sr <= sr_next;
+        else
+          next_interval <= '0';
         end if;
       end if;
     end if;
@@ -87,5 +134,5 @@ begin
   slope_r <= std_logic_vector(sr(41 downto 10));
   ready <= next_interval;
   
-  d0 <= std_logic_vector(ix);
+  d0 <= x"0000" & std_logic_vector(adifl);
 end;
