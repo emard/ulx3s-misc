a47fe6ab563da8bc0c434de8edc116ca3abf9f8f
Emard <vordah@gmail.com>
2021-07-21 16:55:50 +0200

    snap to 100m, needs cleanup

--- tools/wav2kml.py
+++ tools/wav2kml.py
@@ -45,6 +45,14 @@ def distance(lat1:float, lon1:float, lat2:float, lon2:float) -> float:
   dist=2*R*asin(sqrt(h))
   return dist
 
+def heading(p1, p2):
+        lat1, lon1, lat2, lon2 = map(radians, [p1[0], p1[1], p2[0], p2[1]])
+	# lat1,lon1=[a*pi/180.0 for a in p1]
+	# lat2,lon2=[a*pi/180.0 for a in p2]
+        # Heading = atan2(cos(lat1)*sin(lat2)-sin(lat1)*cos(lat2)*cos(lon2-lon1), sin(lon2-lon1)*cos(lat2))
+        Heading = atan2(cos(lon1)*sin(lon2)-sin(lon1)*cos(lon2)*cos(lat2-lat1), sin(lat2-lat1)*cos(lon2))
+        return degrees(Heading)
+
 # convert value 0-1 to rainbow color pink-red
 # color 0=purple 0.2=blue 0.7=green 0.8=yellow 0.9=orange 1=red
 # returns 0xAABBGGRR color32 format
@@ -59,6 +67,246 @@ def color32(color:float) -> int:
   #print("%f %f %f" % (r,g,b) )
   #print("%08x" % color32)
 
+gps_list = list()
+# ((1234, "2021T15Z", (16,44), 80.0, 90.0), ...)
+# named constant indexes in gps list
+gps_seek      = 0
+gps_datetime  = 1
+gps_lonlat    = 2
+gps_speed_kmh = 3
+gps_heading   = 4
+
+class snap:
+  def __init__(self):
+    # cut data into segment length [m]
+    segment_length = 100.0    # [m]
+    segment_snap   =  10.0    # [m]
+    start_length   =   0.0    # analyze from this length [m]
+    stop_length    =   1.0e9  # analyze up to this length [m]
+
+    self.segment_length = segment_length
+    
+    self.init_gps_tracking()
+    self.init_snap_segments()
+
+    # multiple passes over the
+    # same track will snap to same points
+    # use 0 or negative segment_snap to disable snapping
+    self.segment_snap = segment_snap
+
+    self.start_length = start_length
+    self.stop_length = stop_length
+
+  def init_gps_tracking(self):
+    # sample realtime tracking
+    # after each discontinuity or segment length
+    # we will reset sampletime. from then on,
+    # sample rate increments should be added
+    # don't initialize to 0 here, stamp_find_gaps has done it correctly
+    # self.sample_realtime = 0.0
+    # associative array for gps entries
+    # self.next_gps = { "timestamp" : 0.0, "latlon" : (), "speed" : 0.0 }
+    # (seek, timestamp, lonlat, speed_kmh, heading),
+    self.next_gps = ( (0, None, (), 0.0, 0.0), )
+    self.prev_gps = self.next_gps
+    # self.prev_prev_gps = self.next_gps
+    # track length up to prev_gps coordinate in [m]
+    self.prev_gps_track_length = 0.0
+    # current gps segment length
+    self.current_gps_segment_length = 0.0
+    # track length at which next length-based cut will be done
+    self.cut_at_length = self.prev_gps_track_length + self.segment_length
+
+
+  def init_snap_segments(self):
+    # empty snap lists
+    # consits of latlon and timestamp
+    self.snap_list = list()
+    # cut list is larger than snap list,
+    # it contains each segment cut to be applied
+    self.cut_list = list()
+
+  # walk thru all gps data, create list of interpolated
+  # equidistant points and snap to them when traveling again
+  # over the same path
+  # length = segment length [m] track will be divided into segments 
+  #          of this length
+  # snap = snap distance [m] when two parts of a track come togheter
+  #        closer than this value, they will "snap" into the
+  #        same spatial point. This value should be less than segment
+  #        length and about equal or larger than GPS accuracy.
+  # creates a list of new points to snap
+  # when new point is considered, existing list is searched
+  # if the point can somewhere otherwise it is placed as a new point
+  def snap_segments(self):
+    # print("Snapping to %.1f m segments, snap distance: %.1f m" % (length, snap))
+    #self.init_gps_tracking()
+    # rewind the gps filehandle to start from beginning
+    gps_i = 0 # reset index of gps data (sequential reading)
+
+    # search for nearest snap point
+    distance_m = None
+    nearest_index = None
+    nearest_point = None
+
+    # finite state machine to search for a snap point    
+    snapstate = 0
+    
+    self.next_gps = None
+    
+    # intialize first cut
+    self.cut_at_length = self.start_length
+
+    while True:
+      if gps_i < len(gps_list):
+        gps = gps_list[gps_i]
+        gps_i += 1
+      else:
+        break;
+      # add to the track length using great circle formula
+      # prev_prev_gps_track_length = self.prev_gps_track_length
+      self.prev_gps_track_length += self.current_gps_segment_length
+      self.prev_gps = self.next_gps
+      self.next_gps = gps
+      if not self.prev_gps:
+        self.prev_gps = self.next_gps
+      # current length (between prev_gps and next_gps)
+      # using haversin great circle formula
+      if self.next_gps[gps_datetime] != None and self.prev_gps[gps_datetime] != None:
+        self.current_gps_segment_length = \
+          distance(self.prev_gps[gps_lonlat][1], self.prev_gps[gps_lonlat][0], self.next_gps[gps_lonlat][1], self.next_gps[gps_lonlat][0])
+
+      # search snap list, compare latlon only.
+      # find the nearest element (great circle)
+      # if an element is found within snap [m] then
+      # enter snap mode - in following data,
+      # find the closest point - snap point
+      prev_distance = distance_m
+      prev_nearest_index = nearest_index
+      prev_nearest_point = nearest_point
+      distance_m = None
+      nearest_index = None
+      nearest_point = None
+      index = 0
+      for snap_point in self.snap_list:
+        #print(snap_point)
+        new_distance = distance(snap_point["lonlat"][1], snap_point["lonlat"][0], self.next_gps[gps_lonlat][1], self.next_gps[gps_lonlat][0])
+        # don't consider latest point as snap point
+        # if index < len(self.snap_list)-0:
+          # find the nearest distance and its list index
+        if distance_m == None or new_distance < distance_m:
+            distance_m = new_distance
+            nearest_index = index
+            # current point along the track
+            nearest_point = self.next_gps
+        index += 1
+      cut_index = index # if we don't find nearest previous point, cut to a new point
+      if nearest_index:
+        # print("nearest index ", nearest_index, " distance: ", distance)
+        
+        # calculate nearest_heading
+        nearest_heading = self.snap_list[nearest_index]["heading"]
+
+        # states of the snap
+        # 0. no snap, searching for a close point
+        # 1. found point within snap distance
+        # or successfully falling towards the same snap point
+        # 2. past the snap, we must exit the snap distance
+        # When it is rising, we know that we have passed the snap point,
+        # so TODO: interpolate back in the current gps path to find the
+        # closest point near the snap point - reset the segment length too
+        if snapstate == 0:
+          if distance_m < self.segment_snap:
+            # found a point within snap range
+            if distance_m < prev_distance:
+              snapstate = 1 # approaching a snap point
+        elif snapstate == 1:
+          if distance_m > prev_distance:
+            # distance is rising, we are receding from snap point
+            snapstate = 2 # wait to completely exit this snap area
+            # we are receding from the point
+            # assume prev index was the closest
+            # and prev nearest point is the point!
+            # force the cut,
+            # calculate approximate cut position
+            cut_index = nearest_index
+            if prev_nearest_index and prev_nearest_index == nearest_index:
+              # approximately project point to track
+              self.cut_at_length = self.prev_gps_track_length + self.current_gps_segment_length * prev_distance / (prev_distance + distance_m)
+            else:
+              self.cut_at_length = self.prev_gps_track_length + self.current_gps_segment_length - 1.0e-3
+            #print("snap to ", self.cut_at_length)
+        elif snapstate >= 2:
+          if distance_m > self.segment_snap/2:
+            snapstate = 0
+      else:
+        # no nearest index -> snap state = 0
+        snapstate = 0
+ 
+      # if not found in the snap list:
+      # add to the length
+      # and when the length is exceeded then interpolate
+      # to cut for exactly length [m] segments and add this to
+      # snap list
+      if snapstate != 1: # if not near any snap point
+       while self.prev_gps_track_length + self.current_gps_segment_length >= self.cut_at_length:
+        # simplify code - don't interpolate
+        #tp = [ self.prev_gps_track_length, self.prev_gps_track_length + self.current_gps_segment_length ]
+        #yp = [ [ self.prev_gps[gps_lonlat][0], self.next_gps[gps_lonlat][0] ],
+        #       [ self.prev_gps[gps_lonlat][1], self.next_gps[gps_lonlat][1] ],
+        #       [ self.prev_gps[gps_timestamp], self.next_gps[gps_timestamp] ] ]
+        #yi = [ numpy.interp(self.cut_at_length, tp, y) for y in yp ]
+        #heading_deg = self.heading(self.prev_gps[gps_lonlat], self.next_gps[gps_lonlat])
+        heading_deg = gps[gps_heading]
+        # 0-90 and 270-360 is normal heading
+        # 90-270 is reverse heading
+        direction = 1
+        if nearest_index and snapstate == 2:
+          heading_difference = ((heading_deg - nearest_heading) % 360)
+          # print "heading difference %.02f %.02f" % (heading, nearest_heading)
+          if heading_difference > 90 and heading_difference < 270:
+            direction = -1
+        else:
+          nearest_heading = heading_deg
+        #if direction < 0:
+        #  print "found reverse heading index %d" % cut_index
+        if nearest_index:
+          segment_index = nearest_index
+        else:
+          segment_index = index
+        cut_point = {
+          "index"     : cut_index,
+          "directional_index" : (cut_index + 1) * direction,
+          "lonlat"    : gps[gps_lonlat],
+          "heading"   : nearest_heading, # heading of the first cut
+          "timestamp" : gps[gps_datetime],
+          "length"    : self.cut_at_length,
+          }
+        # print nearest_heading,heading
+        # print snapstate
+        # only small number of points are snap points 
+        if snapstate != 2:
+          self.snap_list.append(cut_point)
+        # cut point is each point along the track
+        self.cut_list.append(cut_point)
+        self.cut_at_length += self.segment_length
+        # we should leave snap distance before
+        # looking for the next snap point
+        snapstate = 3
+
+    # from snap list generate segment list with similar headings
+
+    # before return
+    # rewind the gps filehandle to start from beginning
+    #self.gps_filehandle.seek(0)
+    #self.init_gps_tracking()
+    self.next_snap_index = 0
+    self.next_cut_index = 0
+    gps_i = 0
+    # print self.snap_list
+    # print self.cut_list
+    #print("Snap: %d segment cuts to %d snap points" % (len(self.cut_list), len(self.snap_list)))
+
 segment_m   = 100.0 # m
 discontinuety_m = 50.0 # m don't draw lines longer than this
 
@@ -67,6 +315,7 @@ discontinuety_m = 50.0 # m don't draw lines longer than this
 datetime    = None
 time_1st    = None
 time_last   = None
+lonlat_1st  = None
 
 k = kml.KML()
 ns = '{http://www.opengis.net/kml/2.2}'
@@ -86,9 +335,11 @@ arrow_icon_href="http://maps.google.com/mapfiles/kml/shapes/arrow.png"
 b=bytearray(12)
 mvb=memoryview(b)
 
+
 for wavfile in argv[1:]:
   f = open(wavfile, "rb");
-  f.seek(44+0*12)
+  seek = 44+0*12
+  f.seek(seek)
   i = 0 # for PPS signal tracking
   prev_i = 0
   prev_corr_i = 0
@@ -99,7 +350,6 @@ for wavfile in argv[1:]:
   lonlat      = None
   lonlat_prev = None
   lonlat_diff = None
-  lonlat_1st  = None
   speed_kt    = 0.0
   speed_kmh   = 0.0
   kmh_min     = 999.9
@@ -110,6 +360,7 @@ for wavfile in argv[1:]:
   # set nmea line empty before reading
   nmea=bytearray(0)
   while f.readinto(mvb):
+    seek += 12 # bytes per sample
     a=(b[0]&1) | ((b[2]&1)<<1) | ((b[4]&1)<<2) | ((b[6]&1)<<3) | ((b[8]&1)<<4) | ((b[10]&1)<<5)
     if a != 32:
       c = a
@@ -204,6 +455,9 @@ for wavfile in argv[1:]:
             f2.append(p0)
             kmh_max = 0.0
             kmh_min = 999.9
+          # append to GPS list
+          #gps_list.append({"datetime":datetime, "lonlat":lonlat, "speed":speed_kmh, "heading":heading},)
+          gps_list.append((seek, datetime, lonlat, speed_kmh, heading))
       # delete, consumed
       nmea=bytearray(0)
     i += 1
@@ -211,9 +465,32 @@ for wavfile in argv[1:]:
 if datetime:
   time_last = datetime
 
+# at this point gps_list is filled with all GPS readings
+# snap to 100m segments
+if False:
+  snp = snap()
+  snp.snap_segments()
+  #for pt in snp.cut_list: # every one for statistics
+  for pt in snp.snap_list: # only the unique snap points
+    is0 = styles.IconStyle(ns, "id",
+              color=("%08X" % color32(1.0/red_iri)),
+              scale=2.0,
+              heading=(180+pt["heading"])%360,
+              icon_href=arrow_icon_href)
+    isty0 = styles.Style(styles = [is0])
+    p0 = kml.Placemark(ns, 'id',
+              name=("%.2f" % 1.0),
+              description=pt["timestamp"].decode("utf-8"),
+              styles=[isty0])
+    p0.geometry = Point(pt["lonlat"])
+    t.timestamp, dummy = t.parse_str(pt["timestamp"])
+    p0.timeStamp = t.timestamp
+    f2.append(p0)
+
 # output to string with hard-replace "visibility" to add LookAt tag
-if time_last:
-  print(k.to_string(prettyprint=True).replace(
+if True:
+  if time_last:
+    print(k.to_string(prettyprint=True).replace(
   "<visibility>1</visibility>",
   "<visibility>1</visibility>\n\
     <LookAt>\n\
@@ -228,6 +505,6 @@ if time_last:
         <end>%s</end>\n\
       </TimeSpan>\n\
     </LookAt>" % (lonlat_1st[0], lonlat_1st[1], time_1st.decode("utf-8"), time_last.decode("utf-8"))
-  ))
-else:
-  print(k.to_string(prettyprint=True))
+    ))
+  else:
+    print(k.to_string(prettyprint=True))
