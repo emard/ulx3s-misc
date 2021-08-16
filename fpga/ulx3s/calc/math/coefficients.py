ba4f0dc686f0c1729566bd07e9e4d0232405a535
davor <davor@ssd.lan>
2021-08-16 14:29:21 +0200

    coefficients simplified with numpy matrix operations

--- fpga/ulx3s/calc/math/coefficients.py
+++ fpga/ulx3s/calc/math/coefficients.py
@@ -25,57 +25,38 @@ A = np.array(
    [   -K2,   -C,          K2,     C],
    [     0,    0,           0,     1],
    [ K2/MU, C/MU, -(K1+K2)/MU, -C/MU]]).astype(np.float32)
-A1 = np.identity(N).astype(np.float32)
-A2 = np.zeros((N,N)).astype(np.float32)
-ST = np.identity(N).astype(np.float32)
-PR = np.zeros((N)).astype(np.float32)
+
+B = np.array(
+  [ 0, 0, 0, K1/MU ]).astype(np.float32)
 
 print("dx =", MM, "mm, dt =", T, "sec")
 
 # Build the 'A' matrix from the vehicle parameters and
 # initialize the 'ST' matrix
 
-print("Initialized A")
+print("A")
 print(A)
+print("B")
+print(B)
 
 # Calculate the state transition matrix 'ST' using a Taylor
-# Series expansion.  The variable 'IT' counts iterations in
-# the series.  The variable 'IS' is used to indicate when the
-# series has converged.  (IS=0 means that no changes in 'ST'
-# occured in the current iteration.)
+# Series expansion until required precision is reached.
+# ST = e^(A*dt) -> taylor expansion:
+# ST = I + A*dt + A^2*dt^2/2! + A^3*dt^3/3! + ...
+ST = np.identity(N).astype(np.float32) # starts from identity matrix
+An = A * T # A^n * dt^n / n! (initialized for first loop iteration = A*dt)
+n = 1 # loop counter used for factorial (2! after the first loop iteration)
+while An.any(): # while any element > 0
+  ST += An
+  n += 1
+  An = np.matmul(An, A) * T / n # A^n * dt^n / n! for next iteration
 
-IT = 0
-IS = 1
-while IS:
-  IT = IT + 1
-  IS = 0
-  for J in range(N):
-    for I in range(N):
-      A2[I,J] = 0.0
-      for K in range(N):
-        A2[I,J] = A2[I,J] + A1[I,K] * A[K,J]
-  for J in range(N):
-    for I in range(N):
-      A1[I,J] = A2[I,J] * T / IT
-      if ST[I,J] != ST[I,J] + A1[I,J]:
-        ST[I,J] = ST[I,J] + A1[I,J]
-        IS = 1
-
-# Calculate A matrix inversion
-
-A = np.linalg.inv(A)
-
-print("Inverted A")
-print(A)
+# Calculate A matrix inversion A^-1
+Ainv = np.linalg.inv(A)
 
 # Calculate the Partial Response matrix.
-
-for I in range(N):
-  PR[I] = -A[I,N-1]
-  for J in range(N):
-    PR[I] = PR[I] + A[I,J] * ST[J,N-1]
-  PR[I] = PR[I] * K1 / MU
-# next I
+# PR = A^-1 * (ST - I) * B
+PR = np.matmul(np.matmul(Ainv, (ST - np.identity(N))), B)
 
 print("ST (State Transition matrix):")
 print(ST)
