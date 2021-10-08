f9c32ee72c75867e52cda9eddf359c6dad29efa4
emard <vordah@gmail.com>
2021-10-08 22:37:34 +0200

    passthru with programn exit and i2c bridge

--- examples/esp32_passthru/proj/hdl/esp32_passthru.v
+++ examples/esp32_passthru/proj/hdl/esp32_passthru.v
@@ -4,6 +4,8 @@
 `default_nettype none
 module esp32_passthru
 #(
+  // BTN0 delay PROGRAMN prevents exit immediately
+  parameter C_progndelay = 16, // 2^n clocks
   // time to hold EN down after power up,
   // 0 to disable (normal)
   // 9 or more to reset ESP32 at power up
@@ -11,9 +13,9 @@ module esp32_passthru
   //   wifi_gpio0-15k-3.3V pullup missing
   //   issues uftpd: ESP32 will reset after upload
   //   so flash from uftpd will not return properly
-  C_powerup_en_time = 0,
+  parameter C_powerup_en_time = 0,
   // timeout to release SD lines after programing ESP32
-  C_prog_release_timeout = 26 // default n=26, 2^n / 25MHz = 2.6s
+  parameter C_prog_release_timeout = 26 // default n=26, 2^n / 25MHz = 2.6s
 )
 (
   input        clk_25mhz,
@@ -30,7 +32,10 @@ module esp32_passthru
                wifi_gpio4 , wifi_gpio2 , wifi_gpio0 ,
   inout        wifi_en,
   //inout        wifi_gpio5 // v3.0.x, not available on v3.1.x
-  output       sd_wp // BGA pin exists but not connected on PCB
+  inout        wifi_sda, wifi_scl, // I2C ESP32
+  inout        gpdi_sda, gpdi_scl, // I2C BOARD
+  inout        user_programn, // to exit this bitstream
+  output       nc // BGA pin exists but not connected on PCB
 );
   // TX/RX passthru
   assign ftdi_rxd = wifi_txd;
@@ -76,7 +81,6 @@ module esp32_passthru
   // wifi_gpio12 (must be 0 for esp32-wroom fuse unprogrammed. esp32-wrover-e works for 0 and 1)
 
   //assign wifi_en = S_prog_out[1];
-  // assign wifi_en      = R_prog_release[C_prog_release_timeout] ? 1'bz : S_prog_out[1] & R_powerup_en_time[C_powerup_en_time] & ~btn[1]; // holding BTN1 disables ESP32, releasing BTN1 reboots ESP32
   assign wifi_en      = S_prog_out[1] & R_powerup_en_time[C_powerup_en_time] & ~btn[2] ? 1'bz : 1'b0; // holding BTN2 disables ESP32, releasing BTN2 reboots ESP32
   assign wifi_gpio13  = R_prog_release[C_prog_release_timeout] ? 1'bz : 1'b1;
   assign wifi_gpio12  = R_prog_release[C_prog_release_timeout] ? 1'bz : 1'b0;
@@ -87,9 +91,56 @@ module esp32_passthru
   assign wifi_gpio0   = R_prog_release[C_prog_release_timeout] ? 1'bz : S_prog_out[0];
   //assign wifi_gpio0 = S_prog_out[0] & btn[0]; // holding BTN0 will hold gpio0 LOW, signal for ESP32 to take control
 
-  assign sd_wp = wifi_gpio0  /* | wifi_gpio5 */ // bootstrapping pins pullups
+  assign nc    = wifi_en     | wifi_gpio0  /* | wifi_gpio5 */ // bootstrapping pins pullups
                | wifi_gpio15 | wifi_gpio14 | wifi_gpio13 | wifi_gpio12 | wifi_gpio4 | wifi_gpio2; // bootstrapping and force pullup sd_cmd, sd_clk, sd_d[3:0] to make SD MMC mode work
-  // sd_wp is not connected on PCB, just to prevent optimizer from removing pullups
+  // nc is not connected on PCB, just to prevent optimizer from removing pullups
+
+  // i2c bridge for ESP32 to access onboard RTC
+
+  // slow clock enable pulse 5 MHz
+  localparam bridge_clk_div = 3; // div = 1+2^n, 25/(1+2^2)=5 MHz
+  reg [bridge_clk_div:0] bridge_cnt;
+  always @(posedge clk_25mhz) // 25 MHz
+  begin
+    if(bridge_cnt[bridge_clk_div])
+      bridge_cnt <= 0;
+    else
+      bridge_cnt <= bridge_cnt + 1;
+  end
+  wire clk_bridge_en = bridge_cnt[bridge_clk_div];
+
+  wire [1:0] i2c_sda_i = {gpdi_sda, wifi_sda};
+  wire [1:0] i2c_sda_t;
+  i2c_bridge i2c_sda_bridge_i
+  (
+    .clk(clk_25mhz),
+    .clk_en(clk_bridge_en),
+    .i(i2c_sda_i),
+    .t(i2c_sda_t)
+  );
+  assign gpdi_sda = i2c_sda_t[1] ? 1'bz : 1'b0;
+  assign wifi_sda = i2c_sda_t[0] ? 1'bz : 1'b0;
+
+  wire [1:0] i2c_scl_i = {gpdi_scl, wifi_scl};
+  wire [1:0] i2c_scl_t;
+  i2c_bridge i2c_scl_bridge_i
+  (
+    .clk(clk_25mhz),
+    .clk_en(clk_bridge_en),
+    .i(i2c_scl_i),
+    .t(i2c_scl_t)
+  );
+  assign gpdi_scl = i2c_scl_t[1] ? 1'bz : 1'b0;
+  assign wifi_scl = i2c_scl_t[0] ? 1'bz : 1'b0;
+
+  // BTN0 exit this bitstream
+  // delay prevents immediate exit
+  reg [C_progndelay:0] delay_programn = 0;
+  always @(posedge clk_25mhz)
+    delay_programn <= btn[0] ? 0 : delay_programn+1;
+  assign user_programn = delay_programn[C_progndelay] ? 1'b0 : 1'bz;
+
+  // LED diagnostics
 
   assign led[7] = wifi_en;      // blue
   //assign led[6] = ~R_prog_release[C_prog_release_timeout]; // green LED ON = ESP32 programming
---
+++ examples/esp32_passthru/proj/top/top_esp32_passthru.v
@@ -0,0 +1,106 @@
+`default_nettype none
+
+// define none or one of:
+//`define i2c_bridge_v20
+//`define i2c_bridge_v316
+
+module top_esp32_passthru
+#(
+  // BTN0 delay PROGRAMN prevents exit immediately
+  parameter C_progndelay = 16, // 2^n clocks
+  // time to hold EN down after power up,
+  // 0 to disable (normal)
+  // 9 or more to reset ESP32 at power up
+  //   (for boards where ESP32 doesn't boot at power up)
+  //   wifi_gpio0-15k-3.3V pullup missing
+  //   issues uftpd: ESP32 will reset after upload
+  //   so flash from uftpd will not return properly
+  parameter C_powerup_en_time = 0,
+  // timeout to release SD lines after programing ESP32
+  parameter C_prog_release_timeout = 26 // default n=26, 2^n / 25MHz = 2.6s
+)
+(
+  // Clock
+  input  wire clk_25mhz,
+
+  // LEDs
+  output wire [7:0] led,
+
+  // Buttons
+  input  wire [6:0] btn,
+	
+  // ESP32 passthru UART
+  input  wire ftdi_txd,
+  output wire ftdi_rxd,
+  input  wire ftdi_ndtr,
+  input  wire ftdi_nrts,
+
+  // ESP32 passthru
+  input  wire wifi_txd,
+  output wire wifi_rxd,
+  input  wire wifi_gpio15, wifi_gpio14,
+  inout  wire wifi_gpio13, wifi_gpio12,
+  inout  wire wifi_gpio4 , wifi_gpio2 , wifi_gpio0 ,
+  inout  wire wifi_en,
+  output wire sd_wp, // BGA pin exists but not connected on PCB
+
+  // ESP32 i2c bridge
+  `ifdef i2c_bridge_v20    // ULX3S v2.x.x or v3.0.x
+  inout  wire wifi_gpio16, // i2c sda ESP32 v3.0.x
+  inout  wire wifi_gpio17, // i2c scl ESP32 v3.0.x
+  inout  wire gpdi_sda,    // i2c sda RTC and GPDI
+  inout  wire gpdi_scl,    // i2c scl RTC and GPDI
+  `endif
+  `ifdef i2c_bridge_v316   // ULX3S v3.1.x
+  inout  wire wifi_gpio22, // i2c sda ESP32 v3.1.7
+  inout  wire wifi_gpio21, // i2c scl ESP32 v3.1.7
+  inout  wire gpdi_sda,    // i2c sda RTC and GPDI
+  inout  wire gpdi_scl,    // i2c scl RTC and GPDI
+  `endif
+
+  // Boot
+  inout  wire user_programn
+);
+
+  esp32_passthru
+  #(
+    .C_progndelay(C_progndelay), // 16: normal
+    .C_powerup_en_time(C_powerup_en_time), // 0: normal, 10: v3.1.6 workaround but then uftpd will have problems
+    .C_prog_release_timeout(C_prog_release_timeout) // 26: normal
+  )
+  esp32_passthru_inst
+  (
+    .clk_25mhz(clk_25mhz),
+    .btn(btn),
+    .led(led),
+    .ftdi_txd(ftdi_txd),
+    .ftdi_rxd(ftdi_rxd),
+    .ftdi_ndtr(ftdi_ndtr),
+    .ftdi_nrts(ftdi_nrts),
+    .wifi_txd(wifi_txd),
+    .wifi_rxd(wifi_rxd),
+    .wifi_gpio15(wifi_gpio15),
+    .wifi_gpio14(wifi_gpio14),
+    .wifi_gpio13(wifi_gpio13),
+    .wifi_gpio12(wifi_gpio12),
+    .wifi_gpio4(wifi_gpio4),
+    .wifi_gpio2(wifi_gpio2),
+    .wifi_gpio0(wifi_gpio0),
+    .wifi_en(wifi_en),
+    `ifdef i2c_bridge_v20
+    .wifi_sda(wifi_gpio16),
+    .wifi_scl(wifi_gpio17),
+    .gpdi_sda(gpdi_sda),    // c2c board GPDI and RTC
+    .gpdi_scl(gpdi_scl),    // i2c board GPDI and RTC
+    `endif
+    `ifdef i2c_bridge_v316
+    .wifi_sda(wifi_gpio22),
+    .wifi_scl(wifi_gpio21),
+    .gpdi_sda(gpdi_sda),    // i2c board GPDI and RTC
+    .gpdi_scl(gpdi_scl),    // i2c board GPDI and RTC
+    `endif
+    .user_programn(user_programn),
+    .nc(sd_wp) // BGA pin exists but not connected on PCB
+  );
+
+endmodule
