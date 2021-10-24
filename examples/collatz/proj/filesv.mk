13c9ec182492fc0e93848036cc6e92d7d3230750
emard <vordah@gmail.com>
2021-10-24 10:32:57 +0200

    files list fixed for moved btn_debounce

--- examples/collatz/proj/filesv.mk
+++ examples/collatz/proj/filesv.mk
@@ -4,7 +4,7 @@ $(TOP_MODULE_FILE) \
 ../../hex/decoder/hex_decoder_v.v \
 ../../gray_counter/hdl/v/gray_counter.v \
 ../../spi_display/hdl/spi_display_verilog/lcd_video.v \
-../../../examples/rtc/i2c_master/proj/hdl/btn_debounce.v \
+../../btn_debounce/hdl/btn_debounce.v \
 ../hdl/collatz_conjecture.v \
 
 # convertible with vhd2vl
