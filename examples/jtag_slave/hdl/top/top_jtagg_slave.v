79455ea78b3757dfe7a2f52795f7b4c0452dbba1
emard <vordah@gmail.com>
2021-11-07 20:09:38 +0100

    JTAGG: DVI HEX output

--- examples/jtag_slave/hdl/top/top_jtagg_slave.v
+++ examples/jtag_slave/hdl/top/top_jtagg_slave.v
@@ -1,3 +1,4 @@
+`default_nettype none
 module top_jtagg_slave
 (
   input  wire clk_25mhz,
@@ -8,13 +9,32 @@ module top_jtagg_slave
   output wire oled_mosi,
   output wire oled_dc,
   output wire oled_resn,
+  output wire [3:0] gpdi_dp,
   output wire wifi_gpio0
 );
   assign wifi_gpio0 = btn[0];
   
   wire tck, tms, tdi, tdo;
 
-  assign clk = clk_25mhz;
+  wire w_locked;
+  wire [3:0] clocks;
+  ecp5pll
+  #(
+      .in_hz( 25*1000000),
+    .out0_hz(125*1000000),                 .out0_tol_hz(0),
+    .out1_hz( 25*1000000), .out1_deg(  0), .out1_tol_hz(0),
+    .out2_hz( 25*1000000), .out2_deg(  0), .out2_tol_hz(0),
+    .out3_hz( 25*1000000), .out3_deg(  0), .out3_tol_hz(0)
+  )
+  ecp5pll_inst
+  (
+    .clk_i(clk_25mhz),
+    .clk_o(clocks),
+    .locked(w_locked)
+  );
+  wire clk_shift  = clocks[0];
+  wire clk_pixel  = clocks[1];
+  wire clk        = clk_pixel;
 
   // vendor-specfic "JTAGG" module: passthru JTAG traffic to user bitstream
   wire jtdi, jtck, jshift, jupdate, jce1, jce2, jrstn, jrti1, jrti2;
@@ -86,10 +106,10 @@ module top_jtagg_slave
     .c_data_len(C_display_bits),
     .c_row_bits(4),
     .c_grid_6x8(1), // NOTE: TRELLIS needs -abc9 option to compile
-    .c_font_file("oled_font.mem"),
+    .c_font_file("hex_font.mem"),
     .c_color_bits(c_color_bits)
   )
-  hex_decoder_inst
+  hex_decoder_lcd_instance
   (
     .clk(clk),
     .data(S_display),
@@ -114,7 +134,7 @@ module top_jtagg_slave
     .c_clk_phase(0),
     .c_clk_polarity(1)
   )
-  lcd_video_inst
+  lcd_video_instance
   (
     .reset(~btn[0]),
     .clk_pixel(clk),
@@ -131,4 +151,101 @@ module top_jtagg_slave
   );
   assign oled_csn = w_oled_csn | ~btn[1]; // 7-pin ST7789: oled_csn is connected to BLK (backlight enable pin)
 
+  wire [15:0] color_vga;
+  wire [9:0] beam_x, beam_rx, beam_y;
+  assign beam_rx = 636 - beam_x;
+  // HEX decoder needs reverse X-scan, few pixels adjustment for pipeline delay
+  hex_decoder_v
+  #(
+    .c_data_len(C_display_bits),
+    .c_row_bits(5), // 2**n digits per row (4*2**n bits/row) 3->32, 4->64, 5->128, 6->256
+    .c_grid_6x8(1), // NOTE: TRELLIS needs -abc9 option to compile
+    .c_font_file("hex_font.mem"),
+    .c_x_bits(8),
+    .c_y_bits(5),
+    .c_color_bits(16)
+  )
+  hex_decoder_dvi_instance
+  (
+    .clk(clk),
+    .data(S_display),
+    .x(beam_rx[9:2]),
+    .y(beam_y[6:2]),
+    .color(color_vga)
+  );
+
+  wire vga_hsync, vga_vsync, vga_blank;
+  vga
+  vga_instance
+  (
+    .clk_pixel(clk_pixel),
+    .clk_pixel_ena(1'b1),
+    .test_picture(1'b0),
+    .beam_x(beam_x),
+    .beam_y(beam_y),
+    .vga_hsync(vga_hsync),
+    .vga_vsync(vga_vsync),
+    .vga_blank(vga_blank)
+  );
+
+  wire [7:0] vga_r, vga_g, vga_b;
+  wire [1:0] dvid_red, dvid_green, dvid_blue, dvid_clock;
+
+  assign vga_r = {color_vga[15:11],color_vga[11],color_vga[11],color_vga[11]};
+  assign vga_g = {color_vga[10:5],color_vga[5],color_vga[5]};
+  assign vga_b = {color_vga[4:0],color_vga[0],color_vga[0],color_vga[0]};
+  vga2dvid
+  #(
+    .c_ddr(1'b1),
+    .c_shift_clock_synchronizer(1'b0)
+  )
+  vga2dvid_instance
+  (
+    .clk_pixel(clk_pixel),
+    .clk_shift(clk_shift),
+    .in_red(vga_r),
+    .in_green(vga_g),
+    .in_blue(vga_b),
+    .in_hsync(vga_hsync),
+    .in_vsync(vga_vsync),
+    .in_blank(vga_blank),
+    // single-ended output ready for differential buffers
+    .out_red(dvid_red),
+    .out_green(dvid_green),
+    .out_blue(dvid_blue),
+    .out_clock(dvid_clock)
+  );
+
+  // vendor specific DDR modules
+  // convert SDR 2-bit input to DDR clocked 1-bit output (single-ended)
+  ODDRX1F ddr_clock(
+    .D0(dvid_clock[0]),
+    .D1(dvid_clock[1]),
+    .Q(gpdi_dp[3]),
+    .SCLK(clk_shift),
+    .RST(1'b0));
+
+  ODDRX1F ddr_red(
+    .D0(dvid_red[0]),
+    .D1(dvid_red[1]),
+    .Q(gpdi_dp[2]),
+    .SCLK(clk_shift),
+    .RST(1'b0));
+
+  ODDRX1F ddr_green(
+    .D0(dvid_green[0]),
+    .D1(dvid_green[1]),
+    .Q(gpdi_dp[1]),
+    .SCLK(clk_shift),
+    .RST(1'b0));
+
+  ODDRX1F ddr_blue(
+    .D0(dvid_blue[0]),
+    .D1(dvid_blue[1]),
+    .Q(gpdi_dp[0]),
+    .SCLK(clk_shift),
+    .RST(1'b0));
+
 endmodule
+`default_nettype wire
+
