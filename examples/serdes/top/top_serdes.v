98d4a46b4a65f6cfaeb4d0e443f2dfcd2fe22d3a
emard <vordah@gmail.com>
2021-12-06 01:32:58 +0100

    serdes: more parameters set, nothing works yet

--- examples/serdes/top/top_serdes.v
+++ examples/serdes/top/top_serdes.v
@@ -74,7 +74,7 @@ module top_serdes
   #(
     // ?: not found in TN1261
     .D_MACROPDB(1'b1), // 0: assert power down
-    .D_IB_PWDNB(1'b1), // ?
+    .D_IB_PWDNB(1'b1), // ? (required for TX)
     .D_XGE_MODE(1'b0), // 1: 10Gb XAUI LSM ethernet, 0: depends on channel mode selection
     .D_LOW_MARK(4'd4), // Clock compensation FIFO low water mark, mean is 4'd8
     .D_HIGH_MARK('d12), // Clock compensation FIFO high water mark, mean is 4'd8
@@ -92,23 +92,24 @@ module top_serdes
     .D_BITCLK_FROM_ND_EN(1'b0), // ? pass clock from neighboring dual (ch0->ch1)
     .D_SYNC_LOCAL_EN(1'b1), // ?
     .D_SYNC_ND_EN(1'b0), // ?
-    .D_TX_MAX_RATE(2.5), // TX max data rate 0.27-3.125/5.0
+    .D_TX_MAX_RATE(5.0), // TX max data rate 0.27-3.125/5.0
+    .D_RX_MAX_RATE(5.0), // TX max data rate 0.27-3.125/5.0
     .D_ISETLOS('d0), // ?
     .D_SETIRPOLY_AUX(2'b10), // ?
     .D_SETICONST_AUX(2'b01), // ?
     .D_SETIRPOLY_CH(2'b10), // ?
     .D_SETICONST_CH(2'b10), // ?
-    .D_REQ_ISET(3'b001), // ?
-    .D_PD_ISET(2'b00), // ?
+    .D_REQ_ISET(3'b011), // ?
+    .D_PD_ISET(2'b11), // ?
     .D_DCO_CALIB_TIME_SEL(2'b00), // ?
     .D_CMUSETISCL4VCO(3'b000), // ?
     .D_CMUSETI4VCO(2'b00), // ?
     .D_CMUSETINITVCT(3'b00), // ?
-    .D_CMUSETZGM(3'b000), // ?
+    .D_CMUSETZGM(3'b100), // ?
     .D_CMUSETP2AGM(3'b000), // ?
     .D_CMUSETP1GM(3'b000), // ?
     .D_CMUSETI4CPZ('d3), // ?
-    .D_CMUSETI4CPP('d3), // ?
+    .D_CMUSETI4CPP('d4), // ?
     .D_CMUSETICP4Z(3'b101), // ?
     .D_CMUSETICP4P(2'b01), // ?
     .D_CMUSETBIASI(2'b00), // ?
@@ -170,10 +171,10 @@ module top_serdes
     .CH0_SEL_SD_RX_CLK(1'b1), // buffer driven by: 1: serdes recovered clock, 0: ff_ebrd_clk
     .CH0_FF_RX_H_CLK_EN(1'b0), // 1: enable, 0: disable
     .CH0_FF_RX_F_CLK_DIS(1'b0), // 1: disable, 0: enable
-    .CH0_REQ_LVL_SET(2'b00), // level setting for equalization: 00: 6 dB, 01: 9 dB, 10: 12 dB, 11: not used
+    .CH0_REQ_LVL_SET(2'b10), // level setting for equalization: 00: 6 dB, 01: 9 dB, 10: 12 dB, 11: not used
     .CH0_REQ_EN(1'b1), // receiver equalization: 1: enable, 0: disable
     .CH0_RTERM_RX(5'd19), // RX termination R ohm: 0: 5K, 1: 80, 4: 75, 6: 70, 11: 60, 19: 50, 25: 46, other: reserved
-    .CH0_RXTERM_CM(2'b11), // Common mode voltage for RX input termination: 00: RX input supply, 01: Floating (AC Ground), 10: GND, 11: RX input supply
+    .CH0_RXTERM_CM(2'b01), // Common mode voltage for RX input termination: 00: RX input supply, 01: Floating (AC Ground), 10: GND, 11: RX input supply
     // CHx_PDEN_SEL: this signal is used to disable the phase detector
     // in the CDR during electrical idle.
     // when set to 1 (enabled), checks if los is set.
@@ -241,7 +242,7 @@ module top_serdes
     //.CH0_TXAMPLITUDE('d600), // TX amplitude in mV, acceptable values 100-1300 (steps of 20)
     //.CH0_TXDEPRE(DISABLED), // De-emphasis pre-cursor select: DISABLED, 0-11
     //.CH0_TXDEPOST(DISABLED), // De-emphasis post-cursor select: DISABLED, 0-11
-    .CH0_PROTOCOL("8BSER"), // "8B10B", "G8B10B", "PCIe", "GbE", "SGMII", "XAUI", "SDI", "CPRI", "JESD204", "10BSER", "8BSER", "eDP"
+    .CH0_PROTOCOL("10BSER"), // "8B10B", "G8B10B", "PCIe", "GbE", "SGMII", "XAUI", "SDI", "CPRI", "JESD204", "10BSER", "8BSER", "eDP"
     .CH0_CDR_CNT4SEL(2'b00), // ?
     .CH0_CDR_CNT8SEL(2'b00), // ?
     .CH0_DCOATDCFG(2'b00), // ?
@@ -288,7 +289,7 @@ module top_serdes
     .CH1_INVERT_RX(1'b0), // received data: 1: invert, 0: normal
     // CHx_ENABLE_CG_ALIGN: only valid when operating in uc_mode
     .CH1_RLOS_SEL(1'b1), // 0: Disabled, 1: Enabled (If the channel is being used, this bit should be set to 1)
-    .CH1_ENABLE_CG_ALIGN(1'b1), // continuous comma alignment: 1: enable, 0: disable
+    .CH1_ENABLE_CG_ALIGN(1'b0), // continuous comma alignment: 1: enable, 0: disable
     .CH1_UDF_COMMA_MASK(10'h3ff), // user defined comma mask
     .CH1_UDF_COMMA_A(10'h283), // user defined comma character 'a'
     .CH1_UDF_COMMA_B(10'h17C), // user defined comma character 'b'
@@ -299,20 +300,20 @@ module top_serdes
     .CH1_DEC_BYPASS(1'b1), // 8b10 decoder: 1: bypass, 0: normal operation
     .CH1_CTC_BYPASS(1'b1), // clock toleration compensation: 1: bypass, 0: normal operation
     .CH1_RX_GEAR_BYPASS(1'b0), // PCS Rx gear box: 1: bypass, 0: normal operation
-    .CH1_LSM_DISABLE(1'b0), // Rx link state machine: 1: Disable, 0: Enable
-    .CH1_MIN_IPG_CNT(1'b11), // minimum IPG to enforce
+    .CH1_LSM_DISABLE(1'b1), // Rx link state machine: 1: Disable, 0: Enable
+    .CH1_MIN_IPG_CNT(1'b00), // minimum IPG to enforce
     .CH1_RX_DCO_CK_DIV(3'b000), // VCO output frequency divide by: 00x: x/1, 01x: x/2, 100: x/4, 101: x/8, 110: x/16, 111: x/32
     .CH1_RCV_DCC_EN(1'b0), // receiver coupling: 1: DC, 0: AC
     .CH1_RATE_MODE_RX(1'b0), // RX rate selection: 0: full rate, 1: half rate
     .CH1_RX_DIV11_SEL(1'b0), // RX rate selection SMPTE: 0: full rate, high definition SMPTE, 1: x/11 rate, standard definition SMPTE
     // CHx_SEL_SD_RX_CLK: FPGA bridge write clock and elastic buffer read clock driven by:
     .CH1_SEL_SD_RX_CLK(1'b1), // buffer driven by: 1: serdes recovered clock, 0: ff_ebrd_clk
-    .CH1_FF_RX_H_CLK_EN(1'b0), // 1: enable, 0: disable
-    .CH1_FF_RX_F_CLK_DIS(1'b0), // 1: disable, 0: enable
-    .CH1_REQ_LVL_SET(2'b00), // level setting for equalization: 00: 6 dB, 01: 9 dB, 10: 12 dB, 11: not used
+    .CH1_FF_RX_H_CLK_EN(1'b1), // half clock: 1: enable, 0: disable
+    .CH1_FF_RX_F_CLK_DIS(1'b1), // full clock: 1: disable, 0: enable
+    .CH1_REQ_LVL_SET(2'b10), // level setting for equalization: 00: 6 dB, 01: 9 dB, 10: 12 dB, 11: not used
     .CH1_REQ_EN(1'b1), // receiver equalization: 1: enable, 0: disable
     .CH1_RTERM_RX(5'd19), // RX termination R ohm: 0: 5K, 1: 80, 4: 75, 6: 70, 11: 60, 19: 50, 25: 46, other: reserved
-    .CH1_RXTERM_CM(2'b11), // Common mode voltage for RX input termination: 00: RX input supply, 01: Floating (AC Ground), 10: GND, 11: RX input supply
+    .CH1_RXTERM_CM(2'b01), // Common mode voltage for RX input termination: 00: RX input supply, 01: Floating (AC Ground), 10: GND, 11: RX input supply
     // CHx_PDEN_SEL: this signal is used to disable the phase detector
     // in the CDR during electrical idle.
     // when set to 1 (enabled), checks if los is set.
@@ -328,7 +329,7 @@ module top_serdes
     .CH1_LEQ_OFFSET_TRIM(3'b000), // ?
     // CHx_RLOS_SEL: enables LOS detector output before enabling CDR
     // phase detector after calibration.
-    .CH1_RX_LOS_LVL(3'b100), // Sets differential p-p threshold voltage for loss of signal detection
+    .CH1_RX_LOS_LVL(3'b111), // Sets differential p-p threshold voltage for loss of signal detection
     .CH1_RX_LOS_CEQ(2'b11), // Sets the equalization value at input stage of LOS detector
     .CH1_RX_LOS_HYST_EN(1'b0), // Enables hysteresis in detection treshold level
     .CH1_RX_LOS_EN(1'b1), // Enables LOS of signal detector: 0: Disabled, 1: Enabled
@@ -339,33 +340,34 @@ module top_serdes
     // TX settings
     .CH1_TPWDNB(1'b1), // transmit channel power: 0: power down, 1: power up
     .CH1_INVERT_TX(1'b0), // transmitted data: 1: invert, 0: normal
-    .CH1_SB_BYPASS(1'b0), // invert TX data after SerDes bridge; 1: invert, 0: normal (note: loopback data is inverted)
+    .CH1_SB_BYPASS(1'b1), // invert TX data after SerDes bridge; 1: invert, 0: normal (note: loopback data is inverted)
+    .CH1_WA_BYPASS(1'b1), // word alignment bypass: 1: bypass, 0: normal
     .CH1_ENC_BYPASS(1'b1), // 1: bypass 8b10 encoder, 0: Normal operation
     .CH1_RATE_MODE_TX(1'b0), // rate selection for transmit: 1: full rate, 0: half rate
     // CHx_RTERM_TX: TX resistor termination select. Disabled when PCIe is enabled
     .CH1_RTERM_TX(5'd19), // TX termination R ohm: 0: 5K, 1: 80, 4: 75, 6: 70, 11: 60, 19: 50, 25: 46, other: reserved
     .CH1_TX_GEAR_BYPASS(1'b0), // PCS Tx gear box 1: bypass, 0: Normal operation
-    .CH1_TX_GEAR_MODE(1'b0), // 2:1 gearing for trasmit path on selected channels: 1: enable, 0: disable (no gearing)
+    .CH1_TX_GEAR_MODE(1'b1), // 2:1 gearing for trasmit path on selected channels: 1: enable, 0: disable (no gearing)
     .CH1_TX_CM_SEL(2'b00), // TX output common mode: 00: power down, 01: 0.6V, 10: 0.55V, 11: 0.5V
     .CH1_TDRV_PRE_EN(1'b0), // TX driver pre-emphasis: 1: enable, 0: disable
     .CH1_TDRV_POST_EN(1'b0), // TX driver post-emphasis: 1: enable, 0:disable
     .CH1_TX_PRE_SIGN(1'b0), // TX pre-emphasis sign: 1: not inverted, 0: inverted
     .CH1_TX_POST_SIGN(1'b0), // TX post-emphasis sign: 1: not inverted, 0: inverted
     .CH1_TDRV_SLICE0_SEL(2'b01), // TX drive slice enable for slice0: 00: power down, 01: select main data, 10: select pre data, 11: select post data
-    .CH1_TDRV_SLICE1_SEL(2'b00), // TX drive slice enable for slice1: 00: power down, 01: select main data, 10: select pre data, 11: select post data
+    .CH1_TDRV_SLICE1_SEL(2'b01), // TX drive slice enable for slice1: 00: power down, 01: select main data, 10: select pre data, 11: select post data
     .CH1_TDRV_SLICE2_SEL(2'b01), // TX drive slice enable for slice2: 00: power down, 01: select main data, 10: select pre data, 11: select post data
     .CH1_TDRV_SLICE3_SEL(2'b01), // TX drive slice enable for slice3: 00: power down, 01: select main data, 10: select pre data, 11: select post data
-    .CH1_TDRV_SLICE4_SEL(2'b00), // TX drive slice enable for slice4: 00: power down, 01: select main data, 10: select pre data, 11: select post data
-    .CH1_TDRV_SLICE5_SEL(2'b00), // TX drive slice enable for slice5: 00: power down, 01: select main data, 10: select pre data, 11: select post data
+    .CH1_TDRV_SLICE4_SEL(2'b01), // TX drive slice enable for slice4: 00: power down, 01: select main data, 10: select pre data, 11: select post data
+    .CH1_TDRV_SLICE5_SEL(2'b01), // TX drive slice enable for slice5: 00: power down, 01: select main data, 10: select pre data, 11: select post data
     // TX driver slice current settings:
     // increases the output current of slice by 100 uA
     // which corresponds to 100 mV in output amplitude
     .CH1_TDRV_SLICE0_CUR(3'd3), // slice 0 swing uA: 0: 100, 1: 200, 2: 300, 3: 400, 4: 500, 5: 600, 6:700, 7:800
-    .CH1_TDRV_SLICE1_CUR(3'd0), // slice 1 swing uA: 0: 100, 1: 200, 2: 300, 3: 400, 4: 500, 5: 600, 6:700, 7:800
+    .CH1_TDRV_SLICE1_CUR(3'd3), // slice 1 swing uA: 0: 100, 1: 200, 2: 300, 3: 400, 4: 500, 5: 600, 6:700, 7:800
     .CH1_TDRV_SLICE2_CUR(2'd3), // slice 2 swing uA: 0: 800, 1: 1600, 2: 2400, 3: 3200
-    .CH1_TDRV_SLICE3_CUR(2'd2), // slice 3 swing uA: 0: 800, 1: 1600, 2: 2400, 3: 3200
-    .CH1_TDRV_SLICE4_CUR(2'd0), // slice 4 swing uA: 0: 800, 1: 1600, 2: 2400, 3: 3200
-    .CH1_TDRV_SLICE5_CUR(2'd0), // slice 5 swing uA: 0: 800, 1: 1600, 2: 2400, 3: 3200
+    .CH1_TDRV_SLICE3_CUR(2'd3), // slice 3 swing uA: 0: 800, 1: 1600, 2: 2400, 3: 3200
+    .CH1_TDRV_SLICE4_CUR(2'd3), // slice 4 swing uA: 0: 800, 1: 1600, 2: 2400, 3: 3200
+    .CH1_TDRV_SLICE5_CUR(2'd3), // slice 5 swing uA: 0: 800, 1: 1600, 2: 2400, 3: 3200
     // CHx_TDRV_DAT_SEL: Driver output select
     // 00: Data from Serializer muxed to driver (normal operation)
     // 01: Data rate clock from Serializer muxed to driver
@@ -374,8 +376,8 @@ module top_serdes
     // 11: Serial LB from equalizer to driver if slb_eq2t_en=1
     .CH1_TDRV_DAT_SEL(2'b00),
     .CH1_TX_DIV11_SEL(1'b0), // TX rate selection SMPTE: 0: full rate, high definition SMPTE, 1: x/11 rate, standard definition SMPTE
-    .CH1_FF_TX_H_CLK_EN(1'b0), // 1: enable, 0: disable
-    .CH1_FF_TX_F_CLK_DIS(1'b0), // 1: disable, 0: enable
+    .CH1_FF_TX_H_CLK_EN(1'b1), // Half Clock 1: enable, 0: disable
+    .CH1_FF_TX_F_CLK_DIS(1'b1), // Full Clock 1: disable, 0: enable
     //.CH1_CDR_MAX_RATE(2.5), // Receive max data rate (CDR)
     //.CH1_TXAMPLITUDE('d600), // TX amplitude in mV, acceptable values 100-1300 (steps of 20)
     //.CH1_TXDEPRE(DISABLED), // De-emphasis pre-cursor select: DISABLED, 0-11
@@ -386,18 +388,18 @@ module top_serdes
     .CH1_DCOATDCFG(2'b00), // ?
     .CH1_DCOATDDLY(2'b00), // ?
     .CH1_DCOBYPSATD(1'b1), // ?
-    .CH1_DCOCALDIV(3'b000), // ?
+    .CH1_DCOCALDIV(3'b010), // ?
     .CH1_DCOCTLGI(3'b011), // ?
-    .CH1_DCODISBDAVOID(1'b0), // ?
+    .CH1_DCODISBDAVOID(1'b1), // ?
     .CH1_DCOFLTDAC(2'b00), // ?
-    .CH1_DCOFTNRG(3'b001), // ?
+    .CH1_DCOFTNRG(3'b010), // ?
     .CH1_DCOIOSTUNE(3'b010), // ?
     .CH1_DCOITUNE(2'b00), // ?
     .CH1_DCOITUNE4LSB(3'b010), // ?
     .CH1_DCOIUPDNX2(1'b1), // ?
-    .CH1_DCONUOFLSB(3'b100), // ?
-    .CH1_DCOSCALEI(2'b01), // ?
-    .CH1_DCOSTARTVAL(3'b010), // ?
+    .CH1_DCONUOFLSB(3'b010), // ?
+    .CH1_DCOSCALEI(2'b00), // ?
+    .CH1_DCOSTARTVAL(3'b001), // ?
     .CH1_DCOSTEP(2'b11), // ?
     .CH1_BAND_THRESHOLD('d0), // ?
     .CH1_AUTO_FACQ_EN(1'b1), // ? reset signal to trigger the DCO frequency acquisition process when it's necessary
@@ -447,11 +449,17 @@ module top_serdes
     // RX
     .CH1_HDINP(), .CH1_HDINN(),
     .CH1_RX_REFCLK(clk),
-    .CH1_FF_RXI_CLK(rx1_pclk), .CH1_FF_RX_PCLK(rx1_pclk),
+    .CH1_FF_RXI_CLK(rx1_pclk),
+    .CH1_FF_EBRD_CLK(rx1_pclk),
+    //.CH1_FF_RX_PCLK(rx1_pclk),
+    //.CH1_FF_RX_F_CLK(rx1_pclk), // full clock
+    .CH1_FF_RX_H_CLK(rx1_pclk), // half clock
+    .CH1_FFC_RATE_MODE_RX(1'b0),
     .CH1_FFC_EI_EN(1'b0), .CH1_FFC_SIGNAL_DETECT(1'b0),
     .CH1_FFC_LANE_RX_RST(rx_pcs_rst),
     .CH1_FFC_RRST(rx_ser_rst),
     .CH1_FFC_RXPWDNB(rx_pwrup),
+    .CH1_FFC_RX_GEAR_MODE(1'b1),
     .CH1_FF_RX_D_0(rxd1[0]), .CH1_FF_RX_D_1(rxd1[1]), .CH1_FF_RX_D_2(rxd1[2]), .CH1_FF_RX_D_3(rxd1[3]),
     .CH1_FF_RX_D_4(rxd1[4]), .CH1_FF_RX_D_5(rxd1[5]), .CH1_FF_RX_D_6(rxd1[6]), .CH1_FF_RX_D_7(rxd1[7]),
     .CH1_FFS_RLOS(rx1_los_lol), .CH1_FFS_RLOL(rx1_cdr_lol),
@@ -460,7 +468,10 @@ module top_serdes
     .CH1_HDOUTP(), .CH1_HDOUTN(),
     .CH1_FFC_TXPWDNB(tx_pwrup),
     .CH1_FFC_LANE_TX_RST(tx_pcs_rst),
-    .CH1_FF_TXI_CLK(tx1_pclk), .CH1_FF_TX_PCLK(tx1_pclk),
+    .CH1_FF_TXI_CLK(tx1_pclk),
+    //.CH1_FF_TX_PCLK(tx1_pclk),
+    //.CH1_FF_TX_F_CLK(tx1_pclk), // full clock
+    .CH1_FF_TX_H_CLK(tx1_pclk), // half clock
     .CH1_FF_TX_D_0(txd[0]), .CH1_FF_TX_D_1(txd[1]),  .CH1_FF_TX_D_2(txd[2]),  .CH1_FF_TX_D_3(txd[3]),
     .CH1_FF_TX_D_4(txd[4]), .CH1_FF_TX_D_5(txd[5]),  .CH1_FF_TX_D_6(txd[6]),  .CH1_FF_TX_D_7(txd[7]),
     .CH1_FF_TX_D_8(comma),  .CH1_FF_TX_D_9(1'b0)  ,  .CH1_FF_TX_D_10(1'b0),   .CH0_FF_TX_D_11(1'b0),
