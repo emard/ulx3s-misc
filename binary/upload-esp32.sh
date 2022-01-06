6b14ade8d1af2b89ec7826b2db55b04d53685547
Emard <vordah@gmail.com>
2022-01-06 10:49:03 +0100

    binary upload scripts

--- binary/upload-esp32.sh
+++ binary/upload-esp32.sh
@@ -3,8 +3,8 @@ python3 ~/.arduino15/packages/esp32/tools/esptool_py/3.0.0/esptool.py \
   --chip esp32 --port /dev/ttyUSB0 --baud 921600 \
   --before default_reset --after hard_reset write_flash -z --flash_mode dio \
   --flash_freq 80m --flash_size detect \
-  0xe000  boot_app0.bin \
-  0x1000  bootloader_qio_80m.bin \
+  0xe000  boot_app0.bin@0xe000 \
+  0x1000  bootloader_qio_80m.bin@0x1000 \
   0x10000 esp32btgps.ino.bin@0x10000 \
   0x8000  esp32btgps.ino.partitions.bin@0x8000 
 
--- binary/upload-fpga.sh
+++ binary/upload-fpga.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-fujprog -j flash accelog_diamond.bit
+#fujprog -j flash ulx3s_12f_accelog.bit
+ofl -b ulx3s_dfu ulx3s_12f_accelog.bit
