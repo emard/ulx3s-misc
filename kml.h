4d0f3c402f9fd337f87fd2687efb7de6f82c74be
Emard <vordah@gmail.com>
2022-02-01 00:34:11 +0100

    kml.h structs don't need "description" field

--- kml.h
+++ kml.h
@@ -4,14 +4,14 @@
 struct s_kml_arrow
 {
   float lon, lat, value, left, right, heading, speed_kmh;
-  char *timestamp, *description;
+  char *timestamp;
 };
 extern struct s_kml_arrow x_kml_arrow[];
 
 struct s_kml_line
 {
   float lon[2], lat[2], value, left, right, speed_kmh;
-  char *timestamp, *description;
+  char *timestamp;
 };
 extern struct s_kml_line x_kml_line[];
 
